2f723b7b917956642c097e142f1d247ca9cae6ba
Kamil Amanowicz <kamil.amanowicz@phoenix-rtos.com>
2018-03-23 20:00:26 +0100

    imx6ull uart driver

---
+++ tty/imx6ull-uart/uartdrv.c
@@ -0,0 +1,237 @@
+/*
+ * Phoenix-RTOS
+ *
+ * Operating system kernel
+ *
+ * i.MX6ULL UART driver
+ *
+ * Copyright 2018 Phoenix Systems
+ * Author: Kamil Amanowicz
+ *
+ * This file is part of Phoenix-RTOS.
+ *
+ * %LICENSE%
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/threads.h>
+#include <sys/mman.h>
+#include <sys/interrupt.h>
+#include <sys/platform.h>
+#include <sys/msg.h>
+#include <errno.h>
+#include <string.h>
+
+#include <uartdrv.h>
+
+#include "../../../phoenix-rtos-kernel/include/arch/imx6ull.h"
+
+#define UART_ADDR 0x02020000
+
+uart_t uart = { 0 };
+
+static int uart_write(void *data, size_t size)
+{
+	int i;
+
+	mutexLock(uart.lock);
+
+	/* wait for previous write to end */
+	while (uart.tx_head != uart.tx_tail)
+		condWait(uart.tx_cond, uart.lock, 0);
+
+	/* write contents of the buffer */
+	for (i = 0; i < size; i++)
+		uart.tx_buff[uart.tx_tail++] = *(char *)(data + i);
+
+	/* enable tx ready interrupt */
+	*(uart.base + ucr1) |= 0x2000;
+
+	/* wait for this write to end */
+	while (uart.tx_head != uart.tx_tail)
+		condWait(uart.tx_cond, uart.lock, 0);
+
+	mutexUnlock(uart.lock);
+
+	return i;
+}
+
+int del = 0;
+
+static int uart_read(void *data, size_t size)
+{
+	int i;
+	int head;
+
+	mutexLock(uart.lock);
+
+	for (i = 0; i < size; i++) {
+		while (uart.rx_head == uart.rx_tail)
+			condWait(uart.rx_cond, uart.lock, 0);
+		*(char *)(data + i) = uart.rx_buff[uart.rx_head];
+		head = uart.rx_head + 1;
+		uart.rx_head = (uart.rx_head & ~0xff) + (head & 0xff);
+	}
+
+	mutexUnlock(uart.lock);
+
+	return i;
+}
+
+void uart_thr(void *arg)
+{
+	u32 port = (u32)arg;
+	msg_t msg;
+	unsigned int rid;
+
+	for (;;) {
+
+		if (msgRecv(port, &msg, &rid) < 0) {
+			memset(&msg, 0, sizeof(msg));
+			msgRespond(port, &msg, rid);
+			continue;
+		}
+		switch (msg.type) {
+		case mtOpen:
+			break;
+		case mtWrite:
+			msg.o.io.err = uart_write(msg.i.data, msg.i.size);
+			break;
+		case mtRead:
+			msg.o.io.err = uart_read(msg.o.data, msg.o.size);
+			break;
+		case mtClose:
+			break;
+		}
+
+		msgRespond(port, &msg, rid);
+	}
+	return;
+}
+
+
+static int uart_intr(unsigned int intr, void *data)
+{
+	/* disable tx ready interrupt */
+	*(uart.base + ucr1) &= ~0x2000;
+
+	return uart.cond;
+}
+
+static void uart_intrthr(void *arg)
+{
+	char c;
+	int chr = 0;
+	int tail;
+
+	while (1) {
+
+		mutexLock(uart.lock);
+		if (!(*(uart.base + usr1) & (1 << 9)) && uart.tx_head == uart.tx_tail)
+			condWait(uart.cond, uart.lock, 0);
+
+		if ((*(uart.base + usr1) & (1 << 9))) {
+			c = *(uart.base + urxd);
+
+			if (c == 0xd) {
+				chr = -1;
+				c = 0xa;
+			}
+
+			if (c == 0x8) {
+				c = 0;
+				if (chr > 0) {
+					c = '\b';
+					chr--;
+				}
+			}
+
+			if (c) {
+				chr++;
+				uart.rx_buff[uart.rx_tail] = c;
+				tail = uart.rx_tail + 1;
+				uart.rx_tail = (uart.rx_tail & ~0xff) + (tail & 0xff);
+
+				if (c == '\b') {
+					chr--;
+					uart.tx_buff[uart.tx_tail++] = c;
+					uart.tx_buff[uart.tx_tail++] = ' ';
+				}
+				uart.tx_buff[uart.tx_tail++] = c;
+
+				condSignal(uart.rx_cond);
+			}
+		}
+
+		while (uart.tx_head != uart.tx_tail) {
+			if (*(uart.base + usr1) & (1 << 13))
+				*(uart.base + utxd) = uart.tx_buff[uart.tx_head++];
+		}
+
+		if (uart.tx_head == uart.tx_tail) {
+			uart.tx_head = 0;
+			uart.tx_tail = 0;
+			condSignal(uart.tx_cond);
+		}
+
+		mutexUnlock(uart.lock);
+	}
+}
+
+
+char __attribute__((aligned(8))) stack[2048];
+char __attribute__((aligned(8))) stack0[2048];
+
+void main(void)
+{
+	u32 port;
+	oid_t oid;
+	platformctl_t uart_clk;
+
+	uart.base = mmap(NULL, 0x1000, PROT_WRITE | PROT_READ, MAP_DEVICE, OID_PHYSMEM, UART_ADDR);
+
+	if (uart.base == NULL)
+		return;
+
+	uart_clk.action = pctl_set;
+	uart_clk.type = pctl_devclock;
+	uart_clk.devclock.dev = uart1;
+	uart_clk.devclock.state = 3;
+
+	platformctl(&uart_clk);
+	*(uart.base + ucr2) &= ~0;
+
+	while(!(*(uart.base + ucr2) & 1));
+
+	if (condCreate(&uart.tx_cond) != EOK)
+		return;
+
+	if (condCreate(&uart.rx_cond) != EOK)
+		return;
+
+	if (mutexCreate(&uart.lock) != EOK)
+		return;
+
+	if (condCreate(&uart.cond) != EOK)
+		return;
+
+	interrupt(58, uart_intr, NULL, uart.cond);
+
+	/* enable uart and rx ready interrupt */
+	*(uart.base + ucr1) |= 0x0201;
+
+	/* soft reset, tx&rx enable, 8bit transmit, parity enabled */
+	*(uart.base + ucr2) = 0x4127;
+	*(uart.base + ucr3) = 0x704;
+
+	if (portCreate(&port) != EOK)
+		return;
+
+	if (portRegister(0, "/dev/ttyS0", &oid) != EOK)
+		return;
+
+	beginthread(uart_intrthr, 3, &stack0, 2048, NULL);
+	beginthread(uart_thr, 3, &stack, 2048, (void *)port);
+	uart_thr((void *)port);
+}
---
+++ tty/imx6ull-uart/uartdrv.h
@@ -0,0 +1,36 @@
+/*
+ * Phoenix-RTOS
+ *
+ * Operating system kernel
+ *
+ * i.MX 6ULL UART driver
+ *
+ * Copyright 2018 Phoenix Systems
+ * Author: Kamil Amanowicz
+ *
+ * This file is part of Phoenix-RTOS.
+ *
+ * %LICENSE%
+ */
+
+
+enum { urxd = 0, utxd = 16, ucr1 = 32, ucr2, ucr3, ucr4, ufcr, usr1, usr2,
+	uesc, utim, ubir, ubmr, ubrc, onems, uts, umcr };
+
+
+typedef struct {
+	volatile u32 *base;
+
+	u8 rx_buff[256];
+	int rx_head;
+	int rx_tail;
+	handle_t rx_cond;
+
+	u8 tx_buff[256];
+	int tx_head;
+	int tx_tail;
+	handle_t tx_cond;
+
+	handle_t cond;
+	handle_t lock;
+} uart_t;
