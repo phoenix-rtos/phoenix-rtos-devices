cd4b441b2ffec7ac20a965430f49e6e99ab80c44
Daniel Sawka <daniel.sawka@phoenix-rtos.com>
2018-10-26 20:03:31 +0200

    imx6ull-ecspi: Added library for interfacing with SPI in i.MX

---
+++ spi/imx6ull-ecspi/imx6ull-ecspi.c
@@ -0,0 +1,17 @@
+/*
+ * Phoenix-RTOS
+ *
+ * i.MX 6ULL ECSPI driver
+ *
+ * Copyright 2018 Phoenix Systems
+ * Author: Daniel Sawka
+ *
+ * %LICENSE%
+ */
+
+#include "ecspi-api.h"
+
+int main(void)
+{
+	return 0;
+}
