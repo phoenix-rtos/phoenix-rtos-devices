7c5bf0c0c24f6c69197a5715af4d96b2674247a0
Aleksander Kaminski <aleksander.kaminski@phoenix-rtos.com>
2018-12-07 11:20:48 +0100

    stm32-multi: Allow setting configuration via CFLAGS

--- multi/stm32-multi/config.h
+++ multi/stm32-multi/config.h
@@ -15,18 +15,44 @@
 #ifndef _CONFIG_H_
 #define _CONFIG_H_
 
+#ifndef UART1
 #define UART1 1
+#endif
+
+#ifndef UART2
 #define UART2 1
+#endif
+
+#ifndef UART3
 #define UART3 0
+#endif
+
+#ifndef UART4
 #define UART4 1
+#endif
+
+#ifndef UART5
 #define UART5 0
+#endif
 
+#ifndef UART_CONSOLE
 #define UART_CONSOLE 4
+#endif
 
+#ifndef SPI1
 #define SPI1 1
-#define SPI2 0
-#define SPI3 0
+#endif
 
+#ifndef SPI2
+#define SPI2 0
+#endif
+
+#ifndef SPI3
+#define SPI3 0
+#endif
+
+#ifndef LCD
 #define LCD 1
+#endif
 
 #endif
