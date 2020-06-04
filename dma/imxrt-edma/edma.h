ab02e0bb52499acb8af821c4e0702078d06630f0
Marcin Baran <marcin.baran@phoenix-rtos.com>
2020-06-04 15:49:27 +0200

    imxrt-edma: fix code style and alignment, add headers

--- dma/imxrt-edma/edma.h
+++ dma/imxrt-edma/edma.h
@@ -1,7 +1,24 @@
+/*
+ * Phoenix-RTOS
+ *
+ * i.MX RT eDMA lib
+ *
+ * Copyright 2019 Phoenix Systems
+ * Author: Krystian Wasik
+ *
+ * This file is part of Phoenix-RTOS.
+ *
+ * %LICENSE%
+ */
+
 #ifndef IMXRT_EDMA_H
 #define IMXRT_EDMA_H
 
 #include <stdint.h>
+
+#define EDMA_NUM_OF_CHANNELS        (32)
+#define EDMA_ERROR_IRQ              (16 + 16)
+#define EDMA_CHANNEL_IRQ(channel)   (16 + channel%16)
 
 /* eDMA Transfer Control Descriptor */
 struct edma_tcd_s {
@@ -28,11 +45,6 @@ struct edma_tcd_s {
 	};
 };
 
-#define EDMA_NUM_OF_CHANNELS	(32)
-
-#define EDMA_CHANNEL_IRQ(channel)   (16 + channel%16)
-#define EDMA_ERROR_IRQ			  (16 + 16)
-
 void dmamux_set_source(uint8_t channel, uint8_t source);
 void dmamux_channel_enable(uint8_t channel);
 void dmamux_channel_disable(uint8_t channel);
@@ -40,10 +52,14 @@ int dmamux_channel_is_enabled(uint8_t channel);
 
 int edma_init(int (*error_isr)(unsigned int n, void *arg));
 
-void edma_copy_tcd(const volatile struct edma_tcd_s* src, volatile struct edma_tcd_s* dst);
+void edma_copy_tcd(const volatile struct edma_tcd_s* src,
+	volatile struct edma_tcd_s* dst);
+
 int edma_install_tcd(const volatile struct edma_tcd_s* tcd, uint8_t channel);
 
-int edma_initialize_tcd_ring(const struct edma_tcd_s* prototype, volatile struct edma_tcd_s** tcds, unsigned cnt, unsigned src_offset, unsigned dst_offset);
+int edma_initialize_tcd_ring(const struct edma_tcd_s* prototype,
+	volatile struct edma_tcd_s** tcds, unsigned cnt,
+	unsigned src_offset, unsigned dst_offset);
 
 /* Returns value of SSIZE/DSIZE bits for transfer size passed as a param */
 uint8_t edma_get_tcd_attr_xsize(uint8_t num_of_bytes);
