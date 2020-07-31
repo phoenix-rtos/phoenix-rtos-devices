cf58ddd3bdc73b0c2d261a3dd3caa4e82ea83e33
HBuczynski <hubert.buczynski@phoenix-rtos.com>
2020-07-31 09:47:35 +0200

    imxrt-multi: add uart hardware flow control as parameter.

--- multi/imxrt-multi/config.h
+++ multi/imxrt-multi/config.h
@@ -32,32 +32,64 @@
 #define UART1 1
 #endif
 
+#ifndef UART1_HW_FLOWCTRL
+#define UART1_HW_FLOWCTRL 0
+#endif
+
 #ifndef UART2
 #define UART2 0
+#endif
+
+#ifndef UART2_HW_FLOWCTRL
+#define UART2_HW_FLOWCTRL 0
 #endif
 
 #ifndef UART3
 #define UART3 0
 #endif
 
+#ifndef UART3_HW_FLOWCTRL
+#define UART3_HW_FLOWCTRL 0
+#endif
+
 #ifndef UART4
 #define UART4 0
+#endif
+
+#ifndef UART4_HW_FLOWCTRL
+#define UART4_HW_FLOWCTRL 0
 #endif
 
 #ifndef UART5
 #define UART5 0
 #endif
 
+#ifndef UART5_HW_FLOWCTRL
+#define UART5_HW_FLOWCTRL 0
+#endif
+
 #ifndef UART6
 #define UART6 0
+#endif
+
+#ifndef UART6_HW_FLOWCTRL
+#define UART6_HW_FLOWCTRL 0
 #endif
 
 #ifndef UART7
 #define UART7 0
 #endif
 
+#ifndef UART7_HW_FLOWCTRL
+#define UART7_HW_FLOWCTRL 0
+#endif
+
 #ifndef UART8
 #define UART8 0
+#endif
+
+#ifndef UART8_HW_FLOWCTRL
+#define UART8_HW_FLOWCTRL 0
 #endif
 
 #ifndef UART_CONSOLE
