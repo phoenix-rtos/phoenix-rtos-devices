bd01594c66cffe29c8fc3387c6eaa5eac7c5c64a
Gerard P.Swiderski <gerard.swiderski@phoenix-rtos.com>
2021-02-11 16:14:40 +0100

    usb/imxrt: Change usage of OCRAM to DTCM
    JIRA: RTOS-6, G3-9, G3-23

--- usb/imxrt-usbc/phy.c
+++ usb/imxrt-usbc/phy.c
@@ -3,8 +3,8 @@
  *
  * usbclient - usb device controller driver
  *
- * Copyright 2019 Phoenix Systems
- * Author: Kamil Amanowicz, Hubert Buczynski
+ * Copyright 2019-2021 Phoenix Systems
+ * Author: Kamil Amanowicz, Hubert Buczynski, Gerard Swiderski
  *
  * This file is part of Phoenix-RTOS.
  *
@@ -13,26 +13,55 @@
 
 #include <sys/platform.h>
 #include <phoenix/arch/imxrt.h>
+#include <sys/mman.h>
 
 #include "../imx-usbc/phy.h"
 
 
 #define USB_BASE_ADDR       0x402E0000
-#define OCRAM2_BASE_ADRR    0x20210000
 
 
-static uint8_t counter_OCRAM2 = 0;
+/* Reserve DTCM Memory range. */
+/* NOTICE: Verify if syspage map setup is not overlaping with uncached map
+ * defined below, uncached map should be aligned to 0x800 both base and
+ * end addresses
+ */
+#define UNCACHED_BASE_ADDR 0x20020000
+#define UNCACHED_END_ADDR  0x20028000
+
+
+static void *uncached_ptr = (void *)UNCACHED_END_ADDR;
+
 
 /* TODO: It should be moved into imx-usbc. */
 void *usbclient_allocBuff(uint32_t size)
 {
-	return  (void *)(OCRAM2_BASE_ADRR + size * counter_OCRAM2++);
+	/* TODO: reimplement this stopgap using mmap() with separate, special
+	 * uncached map and forced alignment to 0x800 instead of only PAGE_SIZE
+	 * alignment
+	 */
+
+	if ((uintptr_t)uncached_ptr & 0x7ff)
+		return MAP_FAILED;
+
+	size = (size + 0x7ff) & ~0x7ff;
+
+	if ((uintptr_t)uncached_ptr < size)
+		return MAP_FAILED;
+
+	if ((uintptr_t)uncached_ptr - size < UNCACHED_BASE_ADDR)
+		return MAP_FAILED;
+
+	uncached_ptr -= size;
+
+	return uncached_ptr;
 }
 
 /* TODO: It should be moved into imx-usbc. */
 void usbclient_buffDestory(void *addrs, uint32_t size)
 {
-	counter_OCRAM2 = 0;
+	/* TODO: reimplement this stopgap with munmap() */
+	uncached_ptr = (void *)UNCACHED_END_ADDR;
 }
 
 
