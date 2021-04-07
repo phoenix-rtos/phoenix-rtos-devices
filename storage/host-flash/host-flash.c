f01c96eb6e9ace9f4c0ad6e5f4d074dcbb50bcbd
tomasz korniluk <tomasz.korniluk@phoenix-rtos.com>
2021-04-07 08:31:01 +0200

    host-flash: improved hostflash_write() function
    JIRA: ISC-66

--- storage/host-flash/host-flash.c
+++ storage/host-flash/host-flash.c
@@ -15,155 +15,161 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
 #include "host-flash.h"
 
 static struct {
-    int filefd;
-    size_t flashsz;
-    size_t sectorsz;
+	int filefd;
+	size_t flashsz;
+	size_t sectorsz;
 } hostflash_common;
 
 ssize_t hostflash_read(unsigned int addr, void *buff, size_t bufflen)
 {
-    ssize_t stat;
-    int read = 0;
+	ssize_t stat;
+	int readsz = 0;
 
-    if (addr + bufflen > hostflash_common.flashsz)
-        return -EINVAL;
+	if (addr + bufflen > hostflash_common.flashsz)
+		return -EINVAL;
 
-    while ((stat = pread(hostflash_common.filefd, (void *)((char *)buff + read), bufflen - read, addr + read)) != 0) {
-        if (stat < 0) {
-            if (errno == -EINTR)
-                continue;
+	while ((stat = pread(hostflash_common.filefd, (void *)((char *)buff + readsz), bufflen - readsz, addr + readsz)) != 0) {
+		if (stat < 0) {
+			if (errno == EINTR)
+				continue;
 
-            return stat;
-        }
+			return stat;
+		}
 
-        read += stat;
-        if (read == bufflen)
-            break;
-    }
+		readsz += stat;
+		if (readsz == bufflen)
+			break;
+	}
 
-    return read;
+	return readsz;
 }
 
 
 ssize_t hostflash_write(unsigned int addr, void *buff, size_t bufflen)
 {
-    char currentByte = 0;
-    size_t wrote = 0;
-    ssize_t stat;
+	char tempTab[256];
+	size_t wrote = 0, i;
+	int len;
+	ssize_t stat, readsz;
 
-    if (addr + bufflen > hostflash_common.flashsz)
-        return -EINVAL;
+	if (addr + bufflen > hostflash_common.flashsz)
+		return -EINVAL;
 
-    while (wrote != bufflen) {
-        stat = hostflash_read(addr + wrote, &currentByte, 1);
-        if (stat <= 0)
-            return stat;
+	while (wrote != bufflen) {
+		if ((bufflen - wrote) >= sizeof(tempTab))
+			len = sizeof(tempTab);
+		else
+			len = bufflen - wrote;
 
-        currentByte = currentByte & *((char *)buff + wrote);
-        stat = pwrite(hostflash_common.filefd, &currentByte, 1, addr + wrote);
-        if (stat < 0) {
-            if (errno == -EINTR)
-                continue;
+		readsz = hostflash_read(addr + wrote, tempTab, len);
+		if (readsz <= 0)
+			return readsz;
 
-            return stat;
-        }
+		for (i = 0; i < readsz; i++)
+			tempTab[i] = tempTab[i] & *((char *)buff + wrote + i);
 
-        wrote++;
-    }
+		len = 0;
+		while (len != readsz) {
+			stat = pwrite(hostflash_common.filefd, &tempTab, readsz - len, addr + wrote + len);
+			if (stat < 0) {
+				if (errno == EINTR)
+					continue;
 
-    return (ssize_t)wrote;
+				return stat;
+			}
+			len += stat;
+		}
+		wrote += len;
+	}
+
+	return (ssize_t)wrote;
 }
 
 
 void hostflash_sectorErase(unsigned int addr)
 {
-    char tempTab[256];
-    ssize_t len = sizeof(tempTab);
-    int erased = 0;
-    unsigned int sectorNum;
-    unsigned int sectorAddr;
-    int stat;
+	char tempTab[256];
+	ssize_t len = sizeof(tempTab);
+	int erased = 0;
+	unsigned int sectorAddr;
+	int stat;
 
-    memset(tempTab, 0xFF, sizeof(tempTab));
+	memset(tempTab, 0xff, sizeof(tempTab));
 
-    if (addr < hostflash_common.flashsz) {
-        sectorNum = addr / hostflash_common.sectorsz;
-        sectorAddr = sectorNum * hostflash_common.sectorsz;
-    }
-    else {
-        return;
-    }
+	if (addr >= hostflash_common.flashsz)
+		return;
 
-    while (erased != hostflash_common.sectorsz) {
-        stat = pwrite(hostflash_common.filefd, tempTab, len, sectorAddr + erased);
-        if (stat < 0) {
-            if (errno == -EINTR)
-                continue;
+	sectorAddr = (addr / hostflash_common.sectorsz) * hostflash_common.sectorsz;
 
-            return;
-        }
-        else {
-            len = len - stat;
-            if (len == 0)
-                len = sizeof(tempTab);
-        }
+	while (erased != hostflash_common.sectorsz) {
+		stat = pwrite(hostflash_common.filefd, tempTab, len, sectorAddr + erased);
+		if (stat < 0) {
+			if (errno == EINTR)
+				continue;
 
-        erased += stat;
-    }
+			return;
+		}
 
-    return;
+		len -= stat;
+		if (len == 0)
+			len = sizeof(tempTab);
+
+		erased += stat;
+	}
+
+	return;
 }
 
 
 void hostflash_chipErase(void)
 {
-    char tempTab[256];
-    size_t erased = 0, len = sizeof(tempTab);
-    ssize_t stat;
+	char tempTab[256];
+	size_t erased = 0, len = sizeof(tempTab);
+	ssize_t stat;
 
-    memset(tempTab, 0xFF, sizeof(tempTab));
+	memset(tempTab, 0xff, sizeof(tempTab));
 
-    while (erased != hostflash_common.flashsz) {
-        stat = pwrite(hostflash_common.filefd, tempTab, len, erased);
-        if (stat < 0) {
-            if (errno == -EINTR)
-                continue;
+	while (erased != hostflash_common.flashsz) {
+		stat = pwrite(hostflash_common.filefd, tempTab, len, erased);
+		if (stat < 0) {
+			if (errno == EINTR)
+				continue;
 
-            return;
-        }
-        else {
-            len = len - stat;
-            if (len == 0)
-                len = sizeof(tempTab);
-        }
+			return;
+		}
 
-        erased += stat;
-    }
+		len -= stat;
+		if (len == 0)
+			len = sizeof(tempTab);
 
-    return;
+		erased += stat;
+	}
+
+	return;
 }
 
 
 int hostflash_init(size_t *flashsz, size_t *sectorsz, const char *fileName)
 {
-    int stat;
+	int stat;
 
-    if ((*sectorsz % 2) || (*flashsz % *sectorsz) || (*sectorsz > *flashsz) || *sectorsz < 256 || *flashsz == 0)
-        return -EINVAL;
+	if ((*sectorsz % 2) || (*flashsz % *sectorsz) || (*sectorsz > *flashsz) || *sectorsz < 256 || *flashsz == 0)
+		return -EINVAL;
 
-    hostflash_common.flashsz = *flashsz;
-    hostflash_common.sectorsz = *sectorsz;
-    hostflash_common.filefd = open(fileName, O_CREAT | O_RDWR, 0666);
-    if (hostflash_common.filefd < 0)
-        return hostflash_common.filefd;
-    stat = ftruncate(hostflash_common.filefd, hostflash_common.flashsz);
-    if (stat < 0) {
-        close(hostflash_common.filefd);
-        return stat;
-    }
-    return 0;
+	hostflash_common.flashsz = *flashsz;
+	hostflash_common.sectorsz = *sectorsz;
+	hostflash_common.filefd = open(fileName, O_CREAT | O_RDWR, 0666);
+	if (hostflash_common.filefd < 0)
+		return hostflash_common.filefd;
+	stat = ftruncate(hostflash_common.filefd, hostflash_common.flashsz);
+	if (stat < 0) {
+		close(hostflash_common.filefd);
+		return stat;
+	}
+	return 0;
 }
