ea680913134b34da018a50e3532a45205fd3eb00
Maciej Purski <maciej.purski@phoenix-rtos.com>
2021-08-06 11:27:52 +0200

    umass: Allow multiple msg threads
    JIRA: RTOS-21

--- storage/umass/umass.c
+++ storage/umass/umass.c
@@ -31,6 +31,8 @@
 
 #include "../pc-ata/mbr.h"
 
+#define UMASS_N_MSG_THREADS 1
+
 #define UMASS_SECTOR_SIZE 512
 #define UMASS_MAX_SECTORS 8
 #define UMASS_PART_OFFSET 32
@@ -81,8 +83,7 @@ typedef struct scsi_cdb10 {
 
 
 typedef struct umass_dev {
-	char buffer[UMASS_SECTOR_SIZE];
-	char stack[1024] __attribute__ ((aligned(8)));
+	char buffer[8 * UMASS_SECTOR_SIZE];
 	struct umass_dev *prev, *next;
 	usb_devinfo_t instance;
 	char path[32];
@@ -91,16 +92,20 @@ typedef struct umass_dev {
 	int pipeOut;
 	int id;
 	int tag;
-	int tid;
 	unsigned port;
 	uint32_t partOffset;
 	uint32_t partSize;
+	handle_t lock;
+	int valid;
 } umass_dev_t;
 
 
 static struct {
 	umass_dev_t *devices;
+	char stack[UMASS_N_MSG_THREADS][1024] __attribute__ ((aligned(8)));
 	unsigned drvport;
+	unsigned msgport;
+	handle_t lock;
 } umass_common;
 
 
@@ -114,10 +119,13 @@ static int umass_transmit(umass_dev_t *dev, void *cmd, size_t clen, char *data, 
 {
 	umass_cbw_t cbw = { 0 };
 	umass_csw_t csw = { 0 };
+	int ret = 0, bytes = 0;
+	int dataPipe;
 
 	if (clen > 16)
 		return -1;
 
+	dataPipe = (dir == usb_dir_out) ? dev->pipeOut : dev->pipeIn;
 	cbw.sig = CBW_SIG;
 	cbw.tag = dev->tag++;
 	cbw.dlen = dlen;
@@ -126,23 +134,24 @@ static int umass_transmit(umass_dev_t *dev, void *cmd, size_t clen, char *data, 
 	cbw.clen = clen;
 	memcpy(cbw.cmd, cmd, clen);
 
-	if (usb_transferBulk(dev->pipeOut, &cbw, sizeof(cbw), usb_dir_out) < 0)
-		return -1;
+	if ((ret = usb_transferBulk(dev->pipeOut, &cbw, sizeof(cbw), usb_dir_out)) != sizeof(cbw))
+		return -EIO;
 
+	/* Optional data transfer */
 	if (dlen > 0) {
-		if (usb_transferBulk((dir == usb_dir_in) ? dev->pipeIn : dev->pipeOut, data, dlen, dir) < 0)
-			return -1;
+		if ((ret = usb_transferBulk(dataPipe, data, dlen, dir)) < 0)
+			return ret;
+		bytes = ret;
 	}
 
-	if (usb_transferBulk(dev->pipeIn, &csw, sizeof(csw), usb_dir_in) < 0)
-		return -1;
+	if ((ret = usb_transferBulk(dev->pipeIn, &csw, sizeof(csw), usb_dir_in)) != sizeof(csw))
+		return -EIO;
 
-	if (csw.sig != CSW_SIG || csw.tag != cbw.tag || csw.status != 0) {
-		fprintf(stderr, "umass: transmit fail\n");
-		return -1;
-	}
+	/* Transfered finished, check transfer correctness */
+	if (csw.sig != CSW_SIG || csw.tag != cbw.tag || csw.status != 0)
+		return -EIO;
 
-	return dlen;
+	return bytes;
 }
 
 
@@ -178,6 +187,7 @@ static int umass_check(umass_dev_t *dev)
 static int umass_read(umass_dev_t *dev, offs_t offs, char *buf, size_t len)
 {
 	scsi_cdb10_t readcmd = { .opcode = 0x28 };
+	int ret;
 
 	if ((offs % UMASS_SECTOR_SIZE) || (len % UMASS_SECTOR_SIZE))
 		return -EINVAL;
@@ -185,16 +195,24 @@ static int umass_read(umass_dev_t *dev, offs_t offs, char *buf, size_t len)
 	if (offs + len > dev->partSize * UMASS_SECTOR_SIZE)
 		return -EINVAL;
 
+	len = min(len, sizeof(dev->buffer));
+
 	readcmd.lba = htonl(offs / UMASS_SECTOR_SIZE + dev->partOffset);
 	readcmd.length = htons((uint16_t)(len / UMASS_SECTOR_SIZE));
 
-	return umass_transmit(dev, &readcmd, sizeof(readcmd), buf, len, usb_dir_in);
+	mutexLock(dev->lock);
+	if ((ret = umass_transmit(dev, &readcmd, sizeof(readcmd), dev->buffer, len, usb_dir_in)) > 0)
+		memcpy(buf, dev->buffer, ret);
+	mutexUnlock(dev->lock);
+
+	return ret;
 }
 
 
 static int umass_write(umass_dev_t *dev, offs_t offs, char *buf, size_t len)
 {
 	scsi_cdb10_t writecmd = { .opcode = 0x2a };
+	int ret;
 
 	if ((offs % UMASS_SECTOR_SIZE) || (len % UMASS_SECTOR_SIZE))
 		return -EINVAL;
@@ -202,10 +220,17 @@ static int umass_write(umass_dev_t *dev, offs_t offs, char *buf, size_t len)
 	if (offs + len > dev->partSize * UMASS_SECTOR_SIZE)
 		return -EINVAL;
 
+	len = min(len, sizeof(dev->buffer));
+
 	writecmd.lba = htonl(offs / UMASS_SECTOR_SIZE + dev->partOffset);
 	writecmd.length = htons((uint16_t)(len / UMASS_SECTOR_SIZE));
 
-	return umass_transmit(dev, &writecmd, sizeof(writecmd), buf, len, usb_dir_out);
+	mutexLock(dev->lock);
+	memcpy(dev->buffer, buf, len);
+	ret = umass_transmit(dev, &writecmd, sizeof(writecmd), dev->buffer, len, usb_dir_out);
+	mutexUnlock(dev->lock);
+
+	return ret;
 }
 
 
@@ -223,53 +248,127 @@ static int umass_getattr(umass_dev_t *dev, int type, int *attr)
 	return EOK;
 }
 
+static umass_dev_t *umass_devFind(int id)
+{
+	umass_dev_t *tmp = umass_common.devices;
+
+	if (tmp != NULL) {
+		do {
+			if (tmp->id == id - 1 && tmp->valid)
+				return tmp;
+			tmp = tmp->next;
+		} while (tmp != umass_common.devices);
+	}
+
+	return NULL;
+}
 
 static void umass_msgthr(void *arg)
 {
-	umass_dev_t *dev = (umass_dev_t *)arg;
+	umass_dev_t *dev;
 	unsigned long rid;
 	msg_t msg;
 
-	dev->tid = gettid();
 	for (;;) {
-		if (msgRecv(dev->port, &msg, &rid) < 0)
+		if (msgRecv(umass_common.msgport, &msg, &rid) < 0)
 			break;
 
-		switch (msg.type) {
-		case mtOpen:
-		case mtClose:
+		/* Ignore this msg, as it might have been sent by us after deletion event */
+		if (msg.type == mtUnlink) {
 			msg.o.io.err = EOK;
-			break;
-
-		case mtRead:
-			msg.o.io.err = umass_read(dev, msg.i.io.offs, msg.o.data, msg.o.size);
-			break;
-
-		case mtWrite:
-			msg.o.io.err = umass_write(dev, msg.i.io.offs, msg.i.data, msg.i.size);
-			break;
-
-		case mtGetAttr:
-			umass_getattr(dev, msg.i.attr.type, &msg.o.attr.val);
-			break;
-
-		default:
-			msg.o.io.err = -EINVAL;
+			msgRespond(umass_common.msgport, &msg, rid);
+			continue;
 		}
 
-		msgRespond(dev->port, &msg, rid);
+		mutexLock(umass_common.lock);
+		dev = umass_devFind(msg.i.io.oid.id);
+		mutexUnlock(umass_common.lock);
+
+		if (dev == NULL) {
+			msg.o.io.err = -ENOENT;
+			msgRespond(umass_common.msgport, &msg, rid);
+			fprintf(stderr, "umass: msg type %d to wrong oid: %d\n", msg.type, msg.i.io.oid.id);
+			continue;
+		}
+
+		switch (msg.type) {
+			case mtOpen:
+			case mtClose:
+				msg.o.io.err = EOK;
+				break;
+
+			case mtRead:
+				msg.o.io.err = umass_read(dev, msg.i.io.offs, msg.o.data, msg.o.size);
+				break;
+
+			case mtWrite:
+				msg.o.io.err = umass_write(dev, msg.i.io.offs, msg.i.data, msg.i.size);
+				break;
+
+			case mtGetAttr:
+				umass_getattr(dev, msg.i.attr.type, &msg.o.attr.val);
+				break;
+
+			default:
+				msg.o.io.err = -EINVAL;
+		}
+
+		msgRespond(umass_common.msgport, &msg, rid);
 	}
 
 	endthread();
 }
+
+
+static umass_dev_t *umass_devAlloc(void)
+{
+	umass_dev_t *dev, *tmp;
+
+	/* Try to find a free device entry, otherwise allocate it */
+	dev = NULL;
+	tmp = umass_common.devices;
+	if (tmp != NULL) {
+		do {
+			if (!tmp->valid) {
+				dev = tmp;
+				break;
+			}
+		} while ((tmp = tmp->next) != umass_common.devices);
+	}
+
+	if (dev == NULL) {
+		if ((dev = malloc(sizeof(umass_dev_t))) == NULL) {
+			fprintf(stderr, "umass: Not enough memory\n");
+			return NULL;
+		}
+
+		/* Get next device number */
+		if (umass_common.devices == NULL)
+			dev->id = 0;
+		else
+			dev->id = umass_common.devices->prev->id + 1;
+
+		if (mutexCreate(&dev->lock)) {
+			free(dev);
+			return NULL;
+		}
+
+		snprintf(dev->path, sizeof(dev->path), "/dev/umass%d", dev->id);
+		dev->valid = 0;
+		LIST_ADD(&umass_common.devices, dev);
+	}
+
+	return dev;
+}
+
 
 static int umass_handleInsertion(usb_devinfo_t *insertion)
 {
 	umass_dev_t *dev;
 	oid_t oid;
 
-	if ((dev = malloc(sizeof(umass_dev_t))) == NULL)
-		return -EINVAL;
+	if ((dev = umass_devAlloc()) == NULL)
+		return -ENOMEM;
 
 	dev->instance = *insertion;
 	if ((dev->pipeCtrl = usb_open(insertion, usb_transfer_control, 0)) < 0) {
@@ -298,29 +397,18 @@ static int umass_handleInsertion(usb_devinfo_t *insertion)
 		return -EINVAL;
 	}
 
-	if (portCreate(&dev->port) != 0) {
-		fprintf(stderr, "umass: Can't create port!\n");
-		return -EINVAL;
-	}
-
-	/* Get next device number */
-	if (umass_common.devices == NULL)
-		dev->id = 0;
-	else
-		dev->id = umass_common.devices->prev->id + 1;
-
 	snprintf(dev->path, sizeof(dev->path), "/dev/umass%d", dev->id);
-	oid.port = dev->port;
-	oid.id = dev->id;
+	oid.port = umass_common.msgport;
+	oid.id = dev->id + 1;
 	if (create_dev(&oid, dev->path) != 0) {
 		fprintf(stderr, "usb: Can't create dev!\n");
 		return -EINVAL;
 	}
 
-	LIST_ADD(&umass_common.devices, dev);
-	fprintf(stderr, "umass: New USB Mass Storage device: %s\n", dev->path);
+	dev->valid = 1;
+	fprintf(stderr, "umass: New USB Mass Storage device: %s sectors: %d\n", dev->path, dev->partSize);
 
-	return beginthread(umass_msgthr, 4, dev->stack, sizeof(dev->stack), dev);
+	return 0;
 }
 
 
@@ -332,19 +420,14 @@ static int umass_handleDeletion(usb_deletion_t *del)
 		return 0;
 
 	do {
-		if (dev->instance.bus == del->bus && dev->instance.dev == del->dev &&
+		if (dev->valid && dev->instance.bus == del->bus && dev->instance.dev == del->dev &&
 		    dev->instance.interface == del->interface) {
-			LIST_REMOVE(&umass_common.devices, dev);
-			portDestroy(dev->port);
+			dev->valid = 0;
 			remove(dev->path);
-
- 			while (threadJoin(0) != dev->tid)
-				;
-
-			free(dev);
-			break;
+			fprintf(stderr, "umass: Device removed: %s\n", dev->path);
 		}
-	} while ((dev = dev->next) != umass_common.devices);
+		dev = dev->next;
+	} while (dev != umass_common.devices);
 
 	return 0;
 }
@@ -352,13 +435,20 @@ static int umass_handleDeletion(usb_deletion_t *del)
 
 int main(int argc, char *argv[])
 {
-	int ret;
+	int ret, i;
 	msg_t msg;
 	usb_msg_t *umsg = (usb_msg_t *)msg.i.raw;
 
+	/* Port for communication with the USB stack */
 	if (portCreate(&umass_common.drvport) != 0) {
 		fprintf(stderr, "umass: Can't create port!\n");
-		return -EINVAL;
+		return -ENOMEM;
+	}
+
+	/* Port for communication with driver clients */
+	if (portCreate(&umass_common.msgport) != 0) {
+		fprintf(stderr, "umass: Can't create port!\n");
+		return -ENOMEM;
 	}
 
 	if ((usb_connect(filters, sizeof(filters) / sizeof(filters[0]), umass_common.drvport)) < 0) {
@@ -366,13 +456,27 @@ int main(int argc, char *argv[])
 		return -EINVAL;
 	}
 
+	if (mutexCreate(&umass_common.lock)) {
+		fprintf(stderr, "umass: Can't create mutex!\n");
+		return -ENOMEM;
+	}
+
+	for (i = 0; i < UMASS_N_MSG_THREADS; i++) {
+		if ((ret = beginthread(umass_msgthr, 4, umass_common.stack[i], sizeof(umass_common.stack[i]), NULL)) != 0) {
+			fprintf(stderr, "umass: fail to beginthread ret: %d\n", ret);
+			return -1;
+		}
+	}
+
 	for (;;) {
 		ret = usb_eventsWait(umass_common.drvport, &msg);
 		if (ret != 0)
 			continue;
+		mutexLock(umass_common.lock);
 		switch (umsg->type) {
 			case usb_msg_insertion:
-				umass_handleInsertion(&umsg->insertion);
+				if (umass_handleInsertion(&umsg->insertion) != 0)
+					fprintf(stderr, "umass: Failed to initialize device!\n");
 				break;
 			case usb_msg_deletion:
 				umass_handleDeletion(&umsg->deletion);
@@ -381,6 +485,7 @@ int main(int argc, char *argv[])
 				fprintf(stderr, "umass: Error when receiving event from host\n");
 				break;
 		}
+		mutexUnlock(umass_common.lock);
 	}
 
 	return 0;
