f6c1d0c670ee03b444bb3aaa1b8f2f3c2a697192
Maciej Purski <maciej.purski@phoenix-rtos.com>
2021-12-13 14:16:24 +0100

    libklog: Add kernel log buffer driver
    JIRA: DTR-3

---
+++ libklog/libklog.h
@@ -0,0 +1,23 @@
+/*
+ * Phoenix-RTOS
+ *
+ * Phoenix-RTOS klog driver
+ *
+ * Copyright 2021 Phoenix Systems
+ * Author: Maciej Purski
+ *
+ * This file is part of Phoenix-RTOS.
+ *
+ * %LICENSE%
+ */
+#ifndef _LIBKLOG_H_
+#define _LIBKLOG_H_
+
+
+typedef void (*libklog_write_t)(const char *buf, size_t size);
+
+
+int libklog_init(libklog_write_t clbk, void *ctx);
+
+
+#endif /* _LIBKLOG_H_ */
