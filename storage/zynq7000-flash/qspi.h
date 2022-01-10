d06a44060bdb36e0dc7db3d6207975d9a746c4b6
HBuczynski <hubert.buczynski@phoenix-rtos.com>
2022-01-10 15:12:40 +0100

    zynq7000-flash: add qspi driver
    JIRA: PD-216

---
+++ storage/zynq7000-flash/qspi.h
@@ -0,0 +1,46 @@
+/*
+ * Phoenix-RTOS
+ *
+ * Zynq-7000 Quad-SPI Controller driver
+ *
+ * Copyright 2021 Phoenix Systems
+ * Author: Hubert Buczynski
+ *
+ * This file is part of Phoenix-RTOS.
+ *
+ * %LICENSE%
+ */
+
+
+#ifndef _QUAD_SPI_H_
+#define _QUAD_SPI_H_
+
+#include <stdint.h>
+#include <time.h>
+
+/* Transmit and receive number of data defines in 'size'. If TX Buff is empty, the dummy words are sent,
+ * otherwise txBuff data is sent, returns EOK on success or <0 on error */
+extern ssize_t qspi_transfer(const uint8_t *txBuff, uint8_t *rxBuff, size_t size, time_t timeout);
+
+
+/* Raise CS down, enable controller */
+extern void qspi_start(void);
+
+
+/* Clean up RX fifo, raise CS high and disable controller */
+extern void qspi_stop(void);
+
+
+/* Switch off clocks and qspi controller */
+extern int qspi_deinit(void);
+
+
+/* Initialize controller:
+ * - I/O mode and manual mode,
+ * - little endian
+ * - src clk = 200 MHz, qspi clk = 50 MHz
+ * returns EOK on success <0 on error */
+extern int qspi_init(void);
+
+
+#endif
