c7499cca94113c5a0b51b0195b6bcb5d233a60ee
HBuczynski <hubert.buczynski@phoenix-rtos.com>
2022-04-21 09:11:03 +0200

    zynq7000-flash/tests: update tests after changes in flashdrv
    PD-216

--- storage/zynq7000-flash/tests/flashdrv.c
+++ storage/zynq7000-flash/tests/flashdrv.c
@@ -26,7 +26,7 @@ struct {
 	uint8_t *rxBuff;
 	size_t buffSz;
 
-	const flash_info_t *info;
+	idtree_t strgs;
 } common;
 
 
@@ -45,20 +45,19 @@ static inline void test_initBuffs(size_t sz)
 }
 
 
-static void test_writeSyncVerify(addr_t offs, size_t sz)
+static void test_writeSyncVerify(storage_t *strg, addr_t offs, size_t sz)
 {
 	/* Prepare sample data to write */
 	test_initBuffs(sz);
 
 	/* Write */
-	TEST_ASSERT_GREATER_OR_EQUAL_INT32_MESSAGE(offs + sz, CFI_SIZE_FLASH(common.info->cfi.chipSize), "wrong offset");
-	TEST_ASSERT_EQUAL(sz, flashdrv_write(offs, common.txBuff, sz));
+	TEST_ASSERT_EQUAL(sz, strg->dev->blk->ops->write(strg, offs, common.txBuff, sz));
 
 	/* Synchronize */
-	TEST_ASSERT_EQUAL(EOK, flashdrv_sync());
+	TEST_ASSERT_EQUAL(EOK, strg->dev->blk->ops->sync(strg));
 
 	/* Verify */
-	TEST_ASSERT_EQUAL(sz, flashdrv_read(offs, common.rxBuff, sz, 0));
+	TEST_ASSERT_EQUAL(sz, strg->dev->blk->ops->read(strg, offs, common.rxBuff, sz));
 	TEST_ASSERT_EQUAL_UINT8_ARRAY(common.txBuff, common.rxBuff, sz);
 	if (sz < common.buffSz)
 		TEST_ASSERT_EQUAL(VALUE_DEFAULT_SAMPLE, common.rxBuff[sz]);
@@ -67,27 +66,40 @@ static void test_writeSyncVerify(addr_t offs, size_t sz)
 
 /* Setup */
 
-TEST_GROUP(test_flashdrv);
+TEST_GROUP(test_flashblk);
 
 
-TEST_SETUP(test_flashdrv)
+TEST_SETUP(test_flashblk)
 {
-	int i;
-	size_t sz;
+	int fdrvRes;
+	storage_t *strg;
+
+	common.txBuff = NULL;
+	common.rxBuff = NULL;
+	common.buffSz = SIZE_DEFAULT_BUFF;
+	idtree_init(&common.strgs);
 
 	/* Initialize flash memory */
-	TEST_ASSERT_EQUAL(EOK, flashdrv_init());
+	do {
+		strg = calloc(1, sizeof(storage_t));
+		TEST_ASSERT_NOT_NULL(strg);
 
-	common.info = flashdrv_flashInfo();
-	common.buffSz = SIZE_DEFAULT_BUFF;
+		fdrvRes = flashdrv_init(strg);
+		TEST_ASSERT_GREATER_OR_EQUAL(0, fdrvRes);
 
-	/* Find max sector size */
-	for (i = 0; i < common.info->cfi.regsCount; ++i) {
-		sz = CFI_SIZE_SECTION(common.info->cfi.regs[i].size);
+		TEST_ASSERT_NOT_NULL(strg->dev);
+		TEST_ASSERT_NOT_NULL(strg->dev->blk);
+		TEST_ASSERT_NOT_NULL(strg->dev->blk->ops);
+		TEST_ASSERT_NOT_NULL(strg->dev->blk->ops->sync);
+		TEST_ASSERT_NOT_NULL(strg->dev->blk->ops->write);
+		TEST_ASSERT_NOT_NULL(strg->dev->blk->ops->read);
 
-		if (sz > common.buffSz)
-			common.buffSz = sz;
-	}
+		idtree_alloc(&common.strgs, &strg->node);
+
+		/* Find max sector size */
+		if (strg->size > common.buffSz)
+			common.buffSz = strg->size;
+	} while (fdrvRes > 0);
 
 	/* Initialize buffers */
 	common.rxBuff = malloc(common.buffSz);
@@ -98,297 +110,281 @@ TEST_SETUP(test_flashdrv)
 }
 
 
-TEST_TEAR_DOWN(test_flashdrv)
+TEST_TEAR_DOWN(test_flashblk)
 {
+	rbnode_t *node;
+	storage_t *strg;
+
+	for (node = lib_rbMinimum(common.strgs.root); node != NULL; node = lib_rbNext(node)) {
+		strg = lib_treeof(storage_t, node, node);
+
+		flashdrv_done(strg);
+		idtree_remove(&common.strgs, &strg->node);
+
+		free(strg);
+	}
+
 	free(common.rxBuff);
 	free(common.txBuff);
-
-	TEST_ASSERT_EQUAL(EOK, flashdrv_done());
 }
 
 
 /* Test info about flash */
 
-TEST(test_flashdrv, info_validate)
+TEST(test_flashblk, info_blk)
 {
-	TEST_ASSERT_NOT_NULL(common.info->name);
-	TEST_ASSERT_NOT_EQUAL(0, common.info->cfi.chipSize);
-	TEST_ASSERT_NOT_EQUAL(0, common.info->cfi.pageSize);
-	TEST_ASSERT_NOT_EQUAL(0, common.info->cfi.regsCount);
+	rbnode_t *node;
+	storage_t *strg;
+
+	for (node = lib_rbMinimum(common.strgs.root); node != NULL; node = lib_rbNext(node)) {
+		strg = lib_treeof(storage_t, node, node);
+
+		TEST_ASSERT_NOT_EQUAL(0, strg->size);
+	}
 }
 
 
 /* Test read operation */
 
-TEST(test_flashdrv, read_toInvalBuff)
+TEST(test_flashblk, read_toInvalBuff)
 {
-	TEST_ASSERT_EQUAL(-EINVAL, flashdrv_read(0, NULL, 0x1000, 0));
-}
+	rbnode_t *node;
+	storage_t *strg;
 
+	for (node = lib_rbMinimum(common.strgs.root); node != NULL; node = lib_rbNext(node)) {
+		strg = lib_treeof(storage_t, node, node);
 
-TEST(test_flashdrv, read_fromInvalOffs)
-{
-	TEST_ASSERT_EQUAL(-EINVAL, flashdrv_read(CFI_SIZE_FLASH(common.info->cfi.chipSize), common.txBuff, 0x1, 0));
-}
-
-
-/* Test erase operations */
-
-TEST(test_flashdrv, erase_invalOffs)
-{
-	int i = 0;
-	addr_t offs;
-	size_t sz = 0, regSz = 0;
-
-	/* Check wrong section alligment */
-	for (i = 0; i < common.info->cfi.regsCount; ++i) {
-		regSz += CFI_SIZE_REGION(common.info->cfi.regs[i].size, common.info->cfi.regs[i].count);
-		sz = CFI_SIZE_SECTION(common.info->cfi.regs[i].size);
-
-		/* Offset in the quarter of sector */
-		offs = regSz - sz + (sz / 4);
-		TEST_ASSERT_EQUAL(-EINVAL, flashdrv_sectorErase(offs));
+		TEST_ASSERT_EQUAL(-EINVAL, strg->dev->blk->ops->read(strg, 0, NULL, 0x1000));
 	}
 }
 
 
-TEST(test_flashdrv, erase_outOfMem)
+TEST(test_flashblk, read_fromInvalOffs)
 {
-	/* Erase out of flash memory range */
-	TEST_ASSERT_EQUAL(-EINVAL, flashdrv_sectorErase(CFI_SIZE_FLASH(common.info->cfi.chipSize)));
-}
+	rbnode_t *node;
+	storage_t *strg;
 
+	for (node = lib_rbMinimum(common.strgs.root); node != NULL; node = lib_rbNext(node)) {
+		strg = lib_treeof(storage_t, node, node);
 
-TEST(test_flashdrv, erase_sector)
-{
-	addr_t offs;
-	size_t sz;
-
-	/* Sample offset in nor flash memory */
-	sz = CFI_SIZE_SECTION(common.info->cfi.regs[0].size);
-	offs = (common.info->cfi.regs[0].count - 1) * sz;
-
-	/* Prepare sample data to write */
-	test_initBuffs(sz);
-
-	TEST_ASSERT_EQUAL(sz, flashdrv_write(offs, common.txBuff, sz));
-	TEST_ASSERT_EQUAL(EOK, flashdrv_sectorErase(offs));
-	TEST_ASSERT_EQUAL(sz, flashdrv_read(offs, common.rxBuff, sz, 0));
-	TEST_ASSERT_EACH_EQUAL_UINT8(0xff, common.rxBuff, sz);
-}
-
-
-TEST(test_flashdrv, erase_chip)
-{
-	int i;
-	addr_t offs[3];
-	static const size_t sz = 0x1000;
-
-	offs[0] = 0;
-	offs[1] = CFI_SIZE_FLASH(common.info->cfi.chipSize) / 2;
-	offs[2] = CFI_SIZE_FLASH(common.info->cfi.chipSize) - sz;
-
-	/* Write data at begin, in the middle and at the end of memory */
-	for (i = 0; i < (sizeof(offs) / sizeof(offs[0])); ++i)
-		test_writeSyncVerify(offs[i], sz);
-
-	/* Erase the whole memory */
-	TEST_ASSERT_EQUAL(EOK, flashdrv_chipErase());
-
-	/* Check if data is erased at begin, in the middle and at the end of memory */
-	for (i = 0; i < (sizeof(offs) / sizeof(offs[0])); ++i) {
-		TEST_ASSERT_EQUAL(sz, flashdrv_read(offs[i], common.rxBuff, sz, 0));
-		TEST_ASSERT_EACH_EQUAL_UINT8(0xff, common.rxBuff, sz);
+		TEST_ASSERT_EQUAL(-EINVAL, strg->dev->blk->ops->read(strg, strg->size, common.txBuff, 0x1));
 	}
 }
 
 
 /* Test write & read operations */
 
-TEST(test_flashdrv, write_outOfRange)
+TEST(test_flashblk, write_outOfRange)
 {
-	TEST_ASSERT_EQUAL(-EINVAL, flashdrv_write(CFI_SIZE_FLASH(common.info->cfi.chipSize), common.txBuff, 0x1));
-}
+	rbnode_t *node;
+	storage_t *strg;
 
+	for (node = lib_rbMinimum(common.strgs.root); node != NULL; node = lib_rbNext(node)) {
+		strg = lib_treeof(storage_t, node, node);
 
-TEST(test_flashdrv, write_fromInvalBuff)
-{
-	TEST_ASSERT_EQUAL(-EINVAL, flashdrv_write(0, NULL, 0x1000));
-}
-
-
-TEST(test_flashdrv, write_flashSync)
-{
-	addr_t offs;
-	size_t sectorSz, sz;
-
-	/* Set sample offset in nor flash memory */
-	sectorSz = CFI_SIZE_SECTION(common.info->cfi.regs[0].size);
-	offs = (common.info->cfi.regs[0].count - 1) * sectorSz;
-	sz = sectorSz - (sectorSz / 4);
-
-	/* Data size is not equal sector size, using flashdrv_sync data should be flashed */
-	test_writeSyncVerify(offs, sz);
-}
-
-
-TEST(test_flashdrv, write_flashNonSync)
-{
-	addr_t offs;
-	size_t sectorSz, sz;
-
-	/* Sample offset in nor flash memory */
-	sectorSz = CFI_SIZE_SECTION(common.info->cfi.regs[0].size);
-	sz = sectorSz - sectorSz / 4;
-	offs = (common.info->cfi.regs[0].count - 1) * sectorSz;
-
-	/* Prepare sample data to write */
-	test_initBuffs(sz);
-
-	/* Data size is not equal sector size, without flashdrv_sync data should not be flashed */
-	TEST_ASSERT_GREATER_OR_EQUAL_INT32_MESSAGE(offs + sz, CFI_SIZE_FLASH(common.info->cfi.chipSize), "wrong offset");
-	TEST_ASSERT_EQUAL(sz, flashdrv_write(offs, common.txBuff, sz));
-	TEST_ASSERT_EQUAL(sz, flashdrv_read(offs, common.rxBuff, sz, 0));
-	TEST_ASSERT_EQUAL(-EINVAL, memcmp(common.rxBuff, common.txBuff, sz) != 0 ? -EINVAL : 0);
-}
-
-
-TEST(test_flashdrv, write_smallSectorSz)
-{
-	int i;
-	addr_t offs;
-	size_t sz = 0, minSz = (size_t)-1;
-
-	/* Find min section size */
-	for (i = 0; i < common.info->cfi.regsCount; ++i) {
-		sz = CFI_SIZE_SECTION(common.info->cfi.regs[i].size);
-
-		if (sz < minSz)
-			minSz = sz;
+		TEST_ASSERT_EQUAL(-EINVAL, strg->dev->blk->ops->write(strg, strg->size, common.txBuff, 0x1));
 	}
-
-	/* Sample offset in nor flash memory */
-	offs = 14 * minSz;
-
-	/* Data size might not be equaled sector size, using flashdrv_sync data should be flashed */
-	test_writeSyncVerify(offs, sz);
 }
 
 
-TEST(test_flashdrv, write_bigSectorSz)
+TEST(test_flashblk, write_fromInvalBuff)
 {
-	int i;
-	addr_t offs;
-	size_t sz = 0, maxSz = 0;
+	rbnode_t *node;
+	storage_t *strg;
 
-	/* Find max sector size */
-	for (i = 0; i < common.info->cfi.regsCount; ++i) {
-		sz = CFI_SIZE_SECTION(common.info->cfi.regs[i].size);
-		if (sz > maxSz)
-			maxSz = sz;
+	for (node = lib_rbMinimum(common.strgs.root); node != NULL; node = lib_rbNext(node)) {
+		strg = lib_treeof(storage_t, node, node);
+
+		TEST_ASSERT_EQUAL(-EINVAL, strg->dev->blk->ops->write(strg, 0, NULL, 0x1000));
 	}
-
-	/* Sample offset in nor flash memory */
-	offs = 20 * maxSz;
-
-	/* Data size might not be equaled sector size, using flashdrv_sync data should be flashed */
-	test_writeSyncVerify(offs, sz);
 }
 
 
-TEST(test_flashdrv, write_smallChunks)
+TEST(test_flashblk, write_flashSync)
+{
+	rbnode_t *node;
+	storage_t *strg;
+
+	for (node = lib_rbMinimum(common.strgs.root); node != NULL; node = lib_rbNext(node)) {
+		strg = lib_treeof(storage_t, node, node);
+
+		/* Data size and offset are not aligned to typicall values */
+		test_writeSyncVerify(strg, 417, 373);
+	}
+}
+
+
+TEST(test_flashblk, write_flashNonSync)
+{
+	addr_t offs = 317;
+	size_t sz = 47;
+
+	rbnode_t *node;
+	storage_t *strg;
+
+	for (node = lib_rbMinimum(common.strgs.root); node != NULL; node = lib_rbNext(node)) {
+		strg = lib_treeof(storage_t, node, node);
+
+		/* Prepare sample data to write */
+		test_initBuffs(sz);
+
+		/* Data size is not equal sector size, without sync data should not be flashed */
+		TEST_ASSERT_EQUAL(sz, strg->dev->blk->ops->write(strg, offs, common.txBuff, sz));
+		TEST_ASSERT_EQUAL(sz, strg->dev->blk->ops->read(strg, offs, common.rxBuff, sz));
+		TEST_ASSERT_EQUAL(-EINVAL, memcmp(common.rxBuff, common.txBuff, sz) != 0 ? -EINVAL : 0);
+	}
+}
+
+
+TEST(test_flashblk, write_smallSectorSz)
+{
+	addr_t offs;
+	rbnode_t *node;
+	storage_t *strg;
+	static const size_t sz = 0x1000; /* The smallest sector size for NOR flash memories */
+
+	for (node = lib_rbMinimum(common.strgs.root); node != NULL; node = lib_rbNext(node)) {
+		strg = lib_treeof(storage_t, node, node);
+
+		/* Sample offset in nor flash memory */
+		offs = 10 * sz;
+
+		/* Data size might not be equaled sector size, using flashdrv_sync data should be flashed */
+		test_writeSyncVerify(strg, offs, sz);
+	}
+}
+
+
+TEST(test_flashblk, write_bigSectorSz)
+{
+	addr_t offs;
+	rbnode_t *node;
+	storage_t *strg;
+	static const size_t sz = 0x10000;
+
+	for (node = lib_rbMinimum(common.strgs.root); node != NULL; node = lib_rbNext(node)) {
+		strg = lib_treeof(storage_t, node, node);
+
+		/* Sample offset in nor flash memory */
+		offs = 7 * sz;
+		/* Skip small regions */
+		if (offs > strg->size)
+			continue;
+
+		/* Data size might not be equaled sector size, using flashdrv_sync data should be flashed */
+		test_writeSyncVerify(strg, offs, sz);
+	}
+}
+
+
+TEST(test_flashblk, write_smallChunks)
 {
 	int i;
 	static const int offs = 0x100;
 	static const size_t sz = 0x20;
 
-	/* Check different length of input buffer to verify correct interpretation of dummy bytes */
-	for (i = 1; i < sz; i++)
-		test_writeSyncVerify(offs, i);
-}
+	rbnode_t *node;
+	storage_t *strg;
 
+	for (node = lib_rbMinimum(common.strgs.root); node != NULL; node = lib_rbNext(node)) {
+		strg = lib_treeof(storage_t, node, node);
 
-TEST(test_flashdrv, write_bigChunk)
-{
-	size_t flashSz = CFI_SIZE_FLASH(common.info->cfi.chipSize);
-	addr_t offs = flashSz / 4;
-
-	/* Test SIZE_DEFAULT_BUFF data length */
-	test_writeSyncVerify(offs, SIZE_DEFAULT_BUFF);
-}
-
-
-TEST(test_flashdrv, write_pageAtBegin)
-{
-	/* Write and verify page size at the beginning of memory */
-	test_writeSyncVerify(0, CFI_SIZE_PAGE(common.info->cfi.pageSize));
-}
-
-
-TEST(test_flashdrv, write_atEnd)
-{
-	static const size_t sz = 0x1000;
-	addr_t offs = CFI_SIZE_FLASH(common.info->cfi.chipSize) - sz;
-
-	/* Write and verify page size at the end of memory */
-	test_writeSyncVerify(offs, sz);
-}
-
-
-TEST(test_flashdrv, write_betweenSectors)
-{
-	size_t sz = CFI_SIZE_SECTION(common.info->cfi.regs[0].size);
-	addr_t offs = sz * 2 + sz / 2;
-
-	test_writeSyncVerify(offs, sz);
-}
-
-
-TEST(test_flashdrv, write_betweenRegions)
-{
-	addr_t offs;
-	size_t sz;
-
-	/* Verify writing data between regions */
-	if (common.info->cfi.regsCount != 1) {
-		sz = CFI_SIZE_SECTION(common.info->cfi.regs[0].size);
-		offs = CFI_SIZE_REGION(common.info->cfi.regs[0].size, common.info->cfi.regs[0].count) - (sz / 2);
-
-		test_writeSyncVerify(offs, sz);
+		/* Check different length of input buffer to verify correct interpretation of dummy bytes */
+		for (i = 1; i < sz; i++)
+			test_writeSyncVerify(strg, offs, i);
 	}
 }
 
 
-TEST_GROUP_RUNNER(test_flashdrv)
+TEST(test_flashblk, write_bigChunk)
 {
-	RUN_TEST_CASE(test_flashdrv, info_validate);
+	addr_t offs;
+	rbnode_t *node;
+	storage_t *strg;
 
-	RUN_TEST_CASE(test_flashdrv, read_toInvalBuff);
-	RUN_TEST_CASE(test_flashdrv, read_fromInvalOffs);
+	for (node = lib_rbMinimum(common.strgs.root); node != NULL; node = lib_rbNext(node)) {
+		strg = lib_treeof(storage_t, node, node);
+		offs = strg->size / 4;
 
-	RUN_TEST_CASE(test_flashdrv, erase_invalOffs);
-	RUN_TEST_CASE(test_flashdrv, erase_outOfMem);
-	RUN_TEST_CASE(test_flashdrv, erase_sector);
-	RUN_TEST_CASE(test_flashdrv, erase_chip);
+		/* Test SIZE_DEFAULT_BUFF data length */
+		test_writeSyncVerify(strg, offs, SIZE_DEFAULT_BUFF);
+	}
+}
 
-	RUN_TEST_CASE(test_flashdrv, write_outOfRange);
-	RUN_TEST_CASE(test_flashdrv, write_fromInvalBuff);
-	RUN_TEST_CASE(test_flashdrv, write_flashSync);
-	RUN_TEST_CASE(test_flashdrv, write_flashNonSync);
-	RUN_TEST_CASE(test_flashdrv, write_smallSectorSz);
-	RUN_TEST_CASE(test_flashdrv, write_bigSectorSz);
-	RUN_TEST_CASE(test_flashdrv, write_smallChunks);
-	RUN_TEST_CASE(test_flashdrv, write_bigChunk);
-	RUN_TEST_CASE(test_flashdrv, write_pageAtBegin);
-	RUN_TEST_CASE(test_flashdrv, write_atEnd);
-	RUN_TEST_CASE(test_flashdrv, write_betweenSectors);
-	RUN_TEST_CASE(test_flashdrv, write_betweenRegions);
+
+TEST(test_flashblk, write_pageAtBegin)
+{
+	rbnode_t *node;
+	storage_t *strg;
+
+	for (node = lib_rbMinimum(common.strgs.root); node != NULL; node = lib_rbNext(node)) {
+		strg = lib_treeof(storage_t, node, node);
+
+		/* Write and verify page size at the beginning of memory */
+		test_writeSyncVerify(strg, 0, 0x200);
+	}
+}
+
+
+TEST(test_flashblk, write_atEnd)
+{
+	addr_t offs;
+	rbnode_t *node;
+	storage_t *strg;
+	static const size_t sz = 0x1000;
+
+	for (node = lib_rbMinimum(common.strgs.root); node != NULL; node = lib_rbNext(node)) {
+		strg = lib_treeof(storage_t, node, node);
+		offs = strg->size - sz;
+
+		/* Write and verify page size at the end of memory */
+		test_writeSyncVerify(strg, offs, sz);
+	}
+}
+
+
+TEST(test_flashblk, write_betweenSectors)
+{
+	size_t sz;
+	rbnode_t *node;
+	storage_t *strg;
+
+	for (node = lib_rbMinimum(common.strgs.root); node != NULL; node = lib_rbNext(node)) {
+		strg = lib_treeof(storage_t, node, node);
+
+		sz = 0x1000;
+		test_writeSyncVerify(strg, sz / 2, sz);
+
+		sz = 0x10000;
+		test_writeSyncVerify(strg, sz / 2, sz);
+	}
+}
+
+
+TEST_GROUP_RUNNER(test_flashblk)
+{
+	RUN_TEST_CASE(test_flashblk, info_blk);
+
+	RUN_TEST_CASE(test_flashblk, read_toInvalBuff);
+	RUN_TEST_CASE(test_flashblk, read_fromInvalOffs);
+
+	RUN_TEST_CASE(test_flashblk, write_outOfRange);
+	RUN_TEST_CASE(test_flashblk, write_fromInvalBuff);
+	RUN_TEST_CASE(test_flashblk, write_flashSync);
+	RUN_TEST_CASE(test_flashblk, write_flashNonSync);
+	RUN_TEST_CASE(test_flashblk, write_smallSectorSz);
+	RUN_TEST_CASE(test_flashblk, write_bigSectorSz);
+	RUN_TEST_CASE(test_flashblk, write_smallChunks);
+	RUN_TEST_CASE(test_flashblk, write_bigChunk);
+	RUN_TEST_CASE(test_flashblk, write_pageAtBegin);
+	RUN_TEST_CASE(test_flashblk, write_atEnd);
+	RUN_TEST_CASE(test_flashblk, write_betweenSectors);
 }
 
 
 void runner(void)
 {
-	RUN_TEST_GROUP(test_flashdrv);
+	RUN_TEST_GROUP(test_flashblk);
 }
 
 
