f999b7302e5b2266e688ccdf82d11252ea63cc1a
Mateusz Niewiadomski <mateusz.niewiadomski@phoenix-rtos.com>
2022-06-20 15:42:52 +0200

    sensors: lsm9dsxx magnetometer driver fixes
    JIRA: PP-6

--- sensors/mag/lsm9dsxx.c
+++ sensors/mag/lsm9dsxx.c
@@ -73,7 +73,7 @@
 
 /* conversions */
 #define MAG4G_CONV_MGAUSS 0.14F        /* conversion from sensor (at +-4 gauss scale) value to milligauss */
-#define MGAUSS_2_UTSL     1.221730475F /* milligauss to microtesla convertion parameter */
+#define MGAUSS_2_UTSL     1.221730475F /* milligauss to microtesla conversion parameter */
 #define UTSL_2_MAGSHUB    10.F         /* microtesla to sensorhub magnetic flux storage scale of 10^-7 T */
 
 
@@ -81,28 +81,28 @@ typedef struct {
 	spimsg_ctx_t spiCtx;
 	sensor_event_t evt;
 	char stack[512] __attribute__((aligned(8)));
-} lsm9dsxxMag_ctx_t;
+} lsm9dsxx_ctx_t;
 
 
 static int16_t translateMag(uint8_t hbyte, uint8_t lbyte)
 {
-	int16_t val = 0;
-	val |= hbyte;
-	val <<= 8;
-	val |= lbyte;
-	return val * MAG4G_CONV_MGAUSS * MGAUSS_2_UTSL * UTSL_2_MAGSHUB; /* value -> milligauss -> microtesla -> sensorhub storage scale*/
+	int16_t val;
+
+	val = (hbyte << 8) | lbyte;
+
+	return val * MAG4G_CONV_MGAUSS * MGAUSS_2_UTSL * UTSL_2_MAGSHUB;
 }
 
 
 static int spiWriteReg(spimsg_ctx_t *spiCtx, uint8_t regAddr, uint8_t regVal)
 {
-	unsigned char cmd[2] = { (regAddr & 0x7F), regVal }; /* write bit set to regAddr */
+	unsigned char cmd[2] = { regAddr, regVal };
 
 	return spimsg_xfer(spiCtx, cmd, sizeof(cmd), NULL, 0, sizeof(cmd));
 }
 
 
-static int lsm9dsxxMag_whoamiCheck(spimsg_ctx_t *spiCtx)
+static int lsm9dsxx_whoamiCheck(spimsg_ctx_t *spiCtx)
 {
 	uint8_t cmd, val, err;
 
@@ -117,10 +117,10 @@ static int lsm9dsxxMag_whoamiCheck(spimsg_ctx_t *spiCtx)
 }
 
 
-static int lsm9dsxxMag_hwSetup(spimsg_ctx_t *spiCtx)
+static int lsm9dsxx_hwSetup(spimsg_ctx_t *spiCtx)
 {
-	if (lsm9dsxxMag_whoamiCheck(spiCtx) != 0) {
-		printf("lsm9dsxxMag: cannot read/wrong WHOAMI returned!\n");
+	if (lsm9dsxx_whoamiCheck(spiCtx) != 0) {
+		printf("lsm9dsxx_mag: cannot read/wrong WHOAMI returned!\n");
 		return -1;
 	}
 
@@ -139,7 +139,7 @@ static int lsm9dsxxMag_hwSetup(spimsg_ctx_t *spiCtx)
 	usleep(10 * 1000);
 
 	/* continuous measurement mode */
-	if (spiWriteReg(spiCtx, REG_CTRL_REG3_M, (VAL_CTRL_REG3_M_MD_CONTINUOUS | VAL_CTRL_REG3_M_SIM_SPIRW | VAL_CTRL_REG3_M_I2CDSBL)) < 0) {
+	if (spiWriteReg(spiCtx, REG_CTRL_REG3_M, (VAL_CTRL_REG3_M_MD_CONTINUOUS)) < 0) {
 		return -1;
 	}
 	usleep(100 * 1000);
@@ -151,9 +151,8 @@ static int lsm9dsxxMag_hwSetup(spimsg_ctx_t *spiCtx)
 static void lsm9dsxx_threadPublish(void *data)
 {
 	int err;
-	time_t tstamp;
 	sensor_info_t *info = (sensor_info_t *)data;
-	lsm9dsxxMag_ctx_t *ctx = info->ctx;
+	lsm9dsxx_ctx_t *ctx = info->ctx;
 	spimsg_ctx_t *spiCtx = &ctx->spiCtx;
 	uint8_t ibuf[SENSOR_OUTPUT_SIZE] = { 0 };
 	uint8_t obuf;
@@ -163,43 +162,40 @@ static void lsm9dsxx_threadPublish(void *data)
 
 		obuf = REG_DATA_OUT | SPI_READ_BIT | SPI_AUTOADDRINCR_BIT;
 		err = spimsg_xfer(spiCtx, &obuf, 1, ibuf, SENSOR_OUTPUT_SIZE, 1);
-		gettime(&tstamp, NULL);
 
 		if (err >= 0) {
-			/* minus accounts for non right-handness of lsm9dsxx accelerometer frame of reference */
+			gettime(&(ctx->evt.timestamp), NULL);
 			ctx->evt.mag.magX = translateMag(ibuf[1], ibuf[0]);
 			ctx->evt.mag.magY = translateMag(ibuf[3], ibuf[2]);
 			ctx->evt.mag.magZ = translateMag(ibuf[5], ibuf[4]);
-			ctx->evt.timestamp = tstamp;
-			/* TEST */
-			printf("%d %d %d\n", ctx->evt.mag.magX, ctx->evt.mag.magY, ctx->evt.mag.magZ);
+
 			sensors_publish(info->id, &ctx->evt);
 		}
 	}
 }
 
 
-static int lsm9dsxxMag_start(sensor_info_t *info)
+static int lsm9dsxx_start(sensor_info_t *info)
 {
 	int err;
-	lsm9dsxxMag_ctx_t *ctx = (lsm9dsxxMag_ctx_t *)info->ctx;
+	lsm9dsxx_ctx_t *ctx = (lsm9dsxx_ctx_t *)info->ctx;
 
 	err = beginthread(lsm9dsxx_threadPublish, 4, ctx->stack, sizeof(ctx->stack), info);
 	if (err >= 0) {
-		printf("lsm9dsxxMag: launched sensor\n");
+		printf("lsm9dsxx_mag: launched sensor\n");
 	}
 
 	return err;
 }
 
 
-static int lsm9dsxxMag_alloc(sensor_info_t *info, const char *args)
+static int lsm9dsxx_alloc(sensor_info_t *info, const char *args)
 {
-	lsm9dsxxMag_ctx_t *ctx;
+	lsm9dsxx_ctx_t *ctx;
 	int ntries = 10;
 
 	/* sensor context allocation */
-	ctx = malloc(sizeof(lsm9dsxxMag_ctx_t));
+	ctx = malloc(sizeof(lsm9dsxx_ctx_t));
 	if (ctx == NULL) {
 		return -ENOMEM;
 	}
@@ -218,7 +214,7 @@ static int lsm9dsxxMag_alloc(sensor_info_t *info, const char *args)
 	while (lookup(args, NULL, &ctx->spiCtx.oid) < 0) {
 		ntries--;
 		if (ntries == 0) {
-			printf("lsm9dsxxMag: Can`t open SPI device\n");
+			printf("lsm9dsxx_mag: Can`t open SPI device\n");
 			free(ctx);
 			return -ETIMEDOUT;
 		}
@@ -226,8 +222,8 @@ static int lsm9dsxxMag_alloc(sensor_info_t *info, const char *args)
 	}
 
 	/* hardware setup of imu */
-	if (lsm9dsxxMag_hwSetup(&ctx->spiCtx) < 0) {
-		printf("lsm9dsxxMag: failed to setup device\n");
+	if (lsm9dsxx_hwSetup(&ctx->spiCtx) < 0) {
+		printf("lsm9dsxx_mag: failed to setup device\n");
 		spimsg_close(&ctx->spiCtx);
 		free(ctx);
 		return -1;
@@ -237,12 +233,12 @@ static int lsm9dsxxMag_alloc(sensor_info_t *info, const char *args)
 }
 
 
-void __attribute__((constructor)) lsm9dsxxMag_register(void)
+void __attribute__((constructor)) lsm9dsxx_mag_register(void)
 {
 	static sensor_drv_t sensor = {
-		.name = "lsm9dsxxMag",
-		.alloc = lsm9dsxxMag_alloc,
-		.start = lsm9dsxxMag_start
+		.name = "lsm9dsxx_mag",
+		.alloc = lsm9dsxx_alloc,
+		.start = lsm9dsxx_start
 	};
 
 	sensors_register(&sensor);
