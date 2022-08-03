phoenix-rtos/phoenix-rtos-devices

commit 2d207385ff1dacf50bf42c645e5901d26656671a

stm32l4-multi: Support klog
JIRA: RTOS-194

multi/stm32l4-multi/tty.c
@@ -16,6 +16,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include <paths.h>
 #include <posix/utils.h>
 #include <sys/pwman.h>
 #include <sys/interrupt.h>
@@ -369,15 +370,29 @@ static void tty_thread(void *arg)
 #endif
 
 
-void tty_log(const char *str)
+ssize_t tty_log(const char *str, size_t len)
 {
 #if CONSOLE_IS_TTY
-	libtty_write(&tty_getCtx(0)->tty_common, str, strlen(str), 0);
+	return libtty_write(&tty_getCtx(0)->tty_common, str, len, 0);
+#endif
+	return 0;
+}
+
+
+void tty_createDev(void)
+{
+#if CONSOLE_IS_TTY
+	oid_t oid;
+
+	oid.port = uart_common.port;
+	oid.id = 0;
+	create_dev(&oid, _PATH_TTY);
+	create_dev(&oid, _PATH_CONSOLE);
 #endif
 }
 
 
-int tty_init(unsigned int *port)
+int tty_init(void)
 {
 #if TTY_CNT != 0
 	unsigned int uart, i;
@@ -397,17 +412,7 @@ int tty_init(unsigned int *port)
 		{ (void *)0x40005000, pctl_uart5, uart5_irq },
 	};
 
-	if (port == NULL)
-		portCreate(&uart_common.port);
-	else
-		uart_common.port = *port;
-
-	oid.port = uart_common.port;
-
-#if CONSOLE_IS_TTY
-	oid.id = 0;
-	create_dev(&oid, "tty");
-#endif
+	portCreate(&uart_common.port);
 
 	for (uart = usart1; uart <= uart5; ++uart) {
 		if (!uartConfig[uart])

multi/stm32l4-multi/Makefile
@@ -9,7 +9,7 @@ LOCAL_PATH := $(call my-dir)
 NAME := stm32l4-multi
 SRCS := $(wildcard $(LOCAL_PATH)*.c)
 LOCAL_HEADERS := stm32l4-multi.h
-DEP_LIBS := libstm32l4-multi libtty
-LIBS := libdummyfs
+DEP_LIBS := libstm32l4-multi libtty libklog
+LIBS := libdummyfs libklog
 
 include $(binary.mk)

multi/stm32l4-multi/stm32l4-multi.c
@@ -21,6 +21,8 @@
 #include <sys/msg.h>
 #include <sys/pwman.h>
 
+#include <libklog.h>
+
 #include "common.h"
 
 #include "adc.h"
@@ -174,8 +176,7 @@ static void handleMsg(msg_t *msg)
 static ssize_t console_write(const char *str, size_t len, int mode)
 {
 #if CONSOLE_IS_TTY
-	tty_log(str);
-	return (ssize_t)len;
+	return tty_log(str, len);
 #else
 	return uart_write(UART_CONSOLE - 1, str, len);
 #endif
@@ -241,20 +242,18 @@ static void thread(void *arg)
 }
 
 
+static void log_write(const char *buff, size_t len)
+{
+	(void)console_write(buff, len, 0);
+}
+
+
 int main(void)
 {
 	int i;
 	oid_t oid;
-	static const char welcome[] = "multidrv: Started\n";
-#if CONSOLE_IS_TTY
-	unsigned int ttyConsolePort;
-#endif
-
 	priority(THREADS_PRIORITY);
 
-#if CONSOLE_IS_TTY
-	portCreate(&ttyConsolePort);
-#endif
 	portCreate(&common.port);
 
 #if BUILTIN_DUMMYFS
@@ -267,11 +266,7 @@ int main(void)
 
 	rcc_init();
 	exti_init();
-#if CONSOLE_IS_TTY
-	tty_init(&ttyConsolePort);
-#else
-	tty_init(NULL);
-#endif
+	tty_init();
 	gpio_init();
 	spi_init();
 	adc_init();
@@ -279,10 +274,14 @@ int main(void)
 	flash_init();
 	i2c_init();
 	uart_init();
+	libklog_init(log_write);
+
+	/* Do this after klog init to keep shell from overtaking klog */
+	tty_createDev();
 
 	portRegister(common.port, "/multi", &oid);
 
-	console_write(welcome, sizeof(welcome) - 1, 0);
+	printf("multidrv: Started\n");
 
 	for (i = 0; i < THREADS_NO - 1; ++i)
 		beginthread(thread, THREADS_PRIORITY, common.stack[i], STACKSZ, (void *)i);

multi/stm32l4-multi/tty.h
@@ -14,11 +14,16 @@
 #ifndef UART_H_
 #define UART_H_
 
+#include <stddef.h>
+#include <stdlib.h> /* FIXME - should be types.h */
 
-void tty_log(const char *str);
+ssize_t tty_log(const char *str, size_t len);
 
 
-int tty_init(unsigned int *port);
+void tty_createDev(void);
+
+
+int tty_init(void);
 
 
 #endif

commit 693a2abee56ad54c615145d729c4fd91d621b651

stm32l4-multi: set oid.port when creating uart devices
This fixes problem introduced in:
2d20738 stm32l4-multi: Support klog

JIRA: ISC-193

multi/stm32l4-multi/tty.c
@@ -413,6 +413,7 @@ int tty_init(void)
 	};
 
 	portCreate(&uart_common.port);
+	oid.port = uart_common.port;
 
 	for (uart = usart1; uart <= uart5; ++uart) {
 		if (!uartConfig[uart])