330d1e54da1bc6aec8b90ad77b1275c68949cd42
Hubert Badocha <hubert.badocha@phoenix-rtos.com>
2023-01-27 13:43:49 +0100

    Changed ADDR macors to offsets

--- spi/imx6ull-qspi/qspi.c
+++ spi/imx6ull-qspi/qspi.c
@@ -41,54 +41,54 @@
 
 #define QSPI_BASE ((void *)0x21e0000)
 
-#define QSPI_LCKCR_ADDR   (qspi_common.base + 0x304 / sizeof(uint32_t))
+#define QSPI_LCKCR        (0x304 / sizeof(uint32_t))
 #define QSPI_LCKCR_LOCK   0x01
 #define QSPI_LCKCR_UNLOCK 0x02
 
-#define QSPI_LUT_KEY_ADDR (qspi_common.base + 0x300 / sizeof(uint32_t))
-#define QSPI_LUT_KEY      0x5af05af0
+#define QSPI_LUTKEY (0x300 / sizeof(uint32_t))
+#define QSPI_LUTKEY_KEY 0x5af05af0
 
-#define QSPI_LUTn_ADDR(n)      (qspi_common.base + (0x310 + 4 * (n)) / sizeof(uint32_t))
+#define QSPI_LUTn(n)           ((0x310 + 4 * (n)) / sizeof(uint32_t))
 #define QSPI_SEQ_START_REGN(n) (4 * (n))
 
-#define QSPI_IPCR_ADDR                    (qspi_common.base + 0x08 / sizeof(uint32_t))
+#define QSPI_IPCR                         (0x08 / sizeof(uint32_t))
 #define QSPI_IPCR_SET_SEQID(reg, seqid)   ((reg) & ~(0x0f << 24)) | ((seqid & 0x0f) << 24)
 #define QSPI_IPCR_SET_IDATSZ(reg, idatsz) ((reg) & ~(0xffff)) | (idatsz)
 
-#define QSPI_FLSHCR_ADDR (qspi_common.base + 0x0C / sizeof(uint32_t))
+#define QSPI_FLSHCR (0x0C / sizeof(uint32_t))
 
-#define QSPI_MCR_ADDR     (qspi_common.base)
+#define QSPI_MCR          (0)
 #define QSPI_MCR_CLR_TXF  (0x01 << 11)
 #define QSPI_MCR_CLR_RXF  (0x01 << 10)
 #define QSPI_MCR_CLR_MDIS (0x01 << 14)
 
-#define QSPI_BFGENCR_ADDR (qspi_common.base + 0x20 / sizeof(uint32_t))
+#define QSPI_BFGENCR (0x20 / sizeof(uint32_t))
 
-#define QSPI_SR_ADDR   (qspi_common.base + 0x15c / sizeof(uint32_t))
+#define QSPI_SR        (0x15c / sizeof(uint32_t))
 #define QSPI_SR_BUSY   (0x01)
 #define QSPI_SR_RXWE   (0x01 << 16)
 #define QSPI_SR_TXEDA  (0x01 << 24)
 #define QSPI_SR_TXFULL (0x01 << 27)
 
-#define QSPI_RBDRn_ADRR(n) (qspi_common.base + (0x200 + 4 * (n)) / sizeof(uint32_t))
+#define QSPI_RBDRn(n) ((0x200 + 4 * (n)) / sizeof(uint32_t))
 
-#define QSPI_RBSR_ADRR    (qspi_common.base + 0x10c / sizeof(uint32_t))
+#define QSPI_RBSR         (0x10c / sizeof(uint32_t))
 #define QSPI_RBSR_TO_READ (((*QSPI_RBSR_ADRR) >> 8) & 0x3f)
 
-#define QSPI_SFAR_ADDR (qspi_common.base + 0x100 / sizeof(uint32_t))
+#define QSPI_SFAR (0x100 / sizeof(uint32_t))
 
-#define QSPI_RBCT_ADDR  (qspi_common.base + 0x110 / sizeof(uint32_t))
+#define QSPI_RBCT       (0x110 / sizeof(uint32_t))
 #define QSPI_RBCT_RXBRD (0x01 << 8)
 
-#define QSPI_FR_ADDR (qspi_common.base + 0x160 / sizeof(uint32_t))
+#define QSPI_FR      (0x160 / sizeof(uint32_t))
 #define QSPI_FR_RBDF (0x01 << 16)
 
-#define QSPI_TBDR_ADDR (qspi_common.base + 0x154 / sizeof(uint32_t))
+#define QSPI_TBDR (0x154 / sizeof(uint32_t))
 
-#define QSPI_SFA1AD (qspi_common.base + 0x180 / sizeof(uint32_t))
-#define QSPI_SFA2AD (qspi_common.base + 0x184 / sizeof(uint32_t))
-#define QSPI_SFB1AD (qspi_common.base + 0x188 / sizeof(uint32_t))
-#define QSPI_SFB2AD (qspi_common.base + 0x18c / sizeof(uint32_t))
+#define QSPI_SFA1AD (0x180 / sizeof(uint32_t))
+#define QSPI_SFA2AD (0x184 / sizeof(uint32_t))
+#define QSPI_SFB1AD (0x188 / sizeof(uint32_t))
+#define QSPI_SFB2AD (0x18c / sizeof(uint32_t))
 
 #define WATERMARK 16
 
@@ -171,32 +171,32 @@ int qspi_setLutSeq(const lut_seq_t *lut, unsigned int lut_seq)
 	if (lut_seq >= NUM_LUT_SEQ) {
 		return -1;
 	}
-	if (*QSPI_LCKCR_ADDR & QSPI_LCKCR_LOCK) {
-		*QSPI_LUT_KEY_ADDR = QSPI_LUT_KEY;
-		*QSPI_LCKCR_ADDR = (*QSPI_LCKCR_ADDR & ~(QSPI_LCKCR_UNLOCK | QSPI_LCKCR_LOCK)) | QSPI_LCKCR_UNLOCK;
+	if (*(qspi_common.base + QSPI_LCKCR) & QSPI_LCKCR_LOCK) {
+		*(qspi_common.base + QSPI_LUTKEY) = QSPI_LUTKEY_KEY;
+		*(qspi_common.base + QSPI_LCKCR) = (*(qspi_common.base + QSPI_LCKCR) & ~(QSPI_LCKCR_UNLOCK | QSPI_LCKCR_LOCK)) | QSPI_LCKCR_UNLOCK;
 	}
 
 	for (int i = 0; i < 4; i++) {
-		*QSPI_LUTn_ADDR(QSPI_SEQ_START_REGN(lut_seq) + i) = (((uint32_t)lut->instrs[2 * i + 1]) << 16) | lut->instrs[2 * i];
+		*(qspi_common.base + QSPI_LUTn(QSPI_SEQ_START_REGN(lut_seq) + i)) = (((uint32_t)lut->instrs[2 * i + 1]) << 16) | lut->instrs[2 * i];
 	}
 
-	*QSPI_LUT_KEY_ADDR = QSPI_LUT_KEY;
-	*QSPI_LCKCR_ADDR = (*QSPI_LCKCR_ADDR & ~(QSPI_LCKCR_UNLOCK | QSPI_LCKCR_LOCK)) | QSPI_LCKCR_LOCK;
+	*(qspi_common.base + QSPI_LUTKEY) = QSPI_LUTKEY_KEY;
+	*(qspi_common.base + QSPI_LCKCR) = (*(qspi_common.base + QSPI_LCKCR) & ~(QSPI_LCKCR_UNLOCK | QSPI_LCKCR_LOCK)) | QSPI_LCKCR_LOCK;
 }
 
 static void set_IPCR(int seq_num, size_t idatsz)
 {
-	*QSPI_IPCR_ADDR = QSPI_IPCR_SET_IDATSZ(QSPI_IPCR_SET_SEQID(*QSPI_IPCR_ADDR, seq_num), idatsz & 0xffff);
+	*(qspi_common.base + QSPI_IPCR) = QSPI_IPCR_SET_IDATSZ(QSPI_IPCR_SET_SEQID(*(qspi_common.base + QSPI_IPCR), seq_num), idatsz & 0xffff);
 }
 
 void qspi_setTCSH(uint8_t cycles)
 {
-	*QSPI_FLSHCR_ADDR = (*QSPI_FLSHCR_ADDR & ~(0x0f << 8)) | (cycles << 8);
+	*(qspi_common.base + QSPI_FLSHCR) = (*(qspi_common.base + QSPI_FLSHCR) & ~(0x0f << 8)) | (cycles << 8);
 }
 
 void qspi_setTCSS(uint8_t cycles)
 {
-	*QSPI_FLSHCR_ADDR = (*QSPI_FLSHCR_ADDR & ~(0x0f)) | cycles;
+	*(qspi_common.base + QSPI_FLSHCR) = (*(qspi_common.base + QSPI_FLSHCR) & ~(0x0f)) | cycles;
 }
 
 int _qspi_readBusy(qspi_dev_t dev, unsigned int lut_seq, uint32_t addr, void *buf, size_t size)
@@ -214,33 +214,31 @@ int _qspi_readBusy(qspi_dev_t dev, unsigned int lut_seq, uint32_t addr, void *bu
 		return -1;
 	}
 
-	*QSPI_RBCT_ADDR = (*QSPI_RBCT_ADDR & ~(0x1f) | WATERMARK);
-
-	*QSPI_SFAR_ADDR = qspi_common.flash_base_addr[dev] + addr;
-
-	*QSPI_MCR_ADDR |= QSPI_MCR_CLR_RXF;
+	*(qspi_common.base + QSPI_RBCT) = (*(qspi_common.base + QSPI_RBCT) & ~(0x1f) | WATERMARK);
+	*(qspi_common.base + QSPI_SFAR) = qspi_common.flash_base_addr[dev] + addr;
+	*(qspi_common.base + QSPI_MCR) |= QSPI_MCR_CLR_RXF;
 
 	set_IPCR(lut_seq, size);
 
 	while (len < size) {
 		do {
-			reg = *QSPI_SR_ADDR;
+			reg = *(qspi_common.base + QSPI_SR);
 		} while ((reg & QSPI_SR_RXWE) == 0 && reg & QSPI_SR_BUSY);
 
 		for (i = 0; i < WATERMARK + 1 && len < size; i++) {
-			reg = *QSPI_RBDRn_ADRR(i);
+			reg = *(qspi_common.base + QSPI_RBDRn(i));
 			for (byte = 0; byte < 4 && len + byte < size; byte++) {
 				((uint8_t *)buf)[len + byte] = reg & 0xff;
 				reg >>= 8;
 			}
 			len += byte;
 		}
-		*QSPI_FR_ADDR |= QSPI_FR_RBDF; /* Buffer pop. */
+		*(qspi_common.base + QSPI_FR) |= QSPI_FR_RBDF; /* Buffer pop. */
 	}
 
-	while (*QSPI_SR_ADDR & QSPI_SR_BUSY)
+	while (*(qspi_common.base + QSPI_SR) & QSPI_SR_BUSY)
 		;
-	*QSPI_MCR_ADDR |= QSPI_MCR_CLR_RXF;
+	*(qspi_common.base + QSPI_MCR) |= QSPI_MCR_CLR_RXF;
 
 	return EOK;
 }
@@ -250,12 +248,12 @@ static size_t write_tx(const void *data, size_t size)
 	size_t byte, sent = 0;
 	uint32_t reg;
 
-	while ((*QSPI_SR_ADDR & QSPI_SR_TXFULL) == 0 && sent < size) {
+	while ((*(qspi_common.base + QSPI_SR) & QSPI_SR_TXFULL) == 0 && sent < size) {
 		reg = 0;
 		for (byte = 0; byte < 4 && sent + byte < size; byte++) {
 			reg |= ((uint8_t *)data)[sent + byte] << (8 * byte);
 		}
-		*QSPI_TBDR_ADDR = reg;
+		*(qspi_common.base + QSPI_TBDR) = reg;
 		sent += byte;
 	}
 
@@ -273,10 +271,10 @@ int _qspi_write(qspi_dev_t dev, unsigned int lut_seq, uint32_t addr, const void 
 		return -1;
 	}
 
-	if (*QSPI_SR_ADDR & QSPI_SR_TXEDA) /* Check if TX buffer is not empty. */
-		*QSPI_MCR_ADDR |= QSPI_MCR_CLR_TXF;
+	if (*(qspi_common.base + QSPI_SR) & QSPI_SR_TXEDA) /* Check if TX buffer is not empty. */
+		*(qspi_common.base + QSPI_MCR) |= QSPI_MCR_CLR_TXF;
 
-	*QSPI_SFAR_ADDR = qspi_common.flash_base_addr[dev] + addr;
+	*(qspi_common.base + QSPI_SFAR) = qspi_common.flash_base_addr[dev] + addr;
 
 	sent += write_tx(data, size);
 
@@ -286,7 +284,7 @@ int _qspi_write(qspi_dev_t dev, unsigned int lut_seq, uint32_t addr, const void 
 		sent += write_tx(data + sent, size - sent);
 	}
 
-	while (*QSPI_SR_ADDR & QSPI_SR_BUSY)
+	while (*(qspi_common.base + QSPI_SR) & QSPI_SR_BUSY)
 		;
 
 	return EOK;
@@ -312,14 +310,14 @@ int _qspi_init(qspi_dev_t dev)
 
 	if (!qspi_common.init) {
 		/* Enable module */
-		*QSPI_MCR_ADDR &= ~QSPI_MCR_CLR_MDIS;
+		*(qspi_common.base + QSPI_MCR) &= ~QSPI_MCR_CLR_MDIS;
 		/* Set write to RXBR buffer */
-		*QSPI_RBCT_ADDR |= QSPI_RBCT_RXBRD;
+		*(qspi_common.base + QSPI_RBCT) |= QSPI_RBCT_RXBRD;
 		/* Set flash sizes */
-		*QSPI_SFA1AD = (*QSPI_SFA1AD & (0x3ff)) | ((QSPI_MMAP_BASE + QSPI_FLASH_A1_SIZE) & ~(0x3ff));
-		*QSPI_SFA2AD = (*QSPI_SFA2AD & (0x3ff)) | ((QSPI_MMAP_BASE + QSPI_FLASH_A2_SIZE) & ~(0x3ff));
-		*QSPI_SFB1AD = (*QSPI_SFB1AD & (0x3ff)) | ((QSPI_MMAP_BASE + QSPI_FLASH_B1_SIZE) & ~(0x3ff));
-		*QSPI_SFB2AD = (*QSPI_SFB2AD & (0x3ff)) | ((QSPI_MMAP_BASE + QSPI_FLASH_B2_SIZE) & ~(0x3ff));
+		*(qspi_common.base + QSPI_SFA1AD) = (*(qspi_common.base + QSPI_SFA1AD) & (0x3ff)) | ((QSPI_MMAP_BASE + QSPI_FLASH_A1_SIZE) & ~(0x3ff));
+		*(qspi_common.base + QSPI_SFA2AD) = (*(qspi_common.base + QSPI_SFA2AD) & (0x3ff)) | ((QSPI_MMAP_BASE + QSPI_FLASH_A2_SIZE) & ~(0x3ff));
+		*(qspi_common.base + QSPI_SFB1AD) = (*(qspi_common.base + QSPI_SFB1AD) & (0x3ff)) | ((QSPI_MMAP_BASE + QSPI_FLASH_B1_SIZE) & ~(0x3ff));
+		*(qspi_common.base + QSPI_SFB2AD) = (*(qspi_common.base + QSPI_SFB2AD) & (0x3ff)) | ((QSPI_MMAP_BASE + QSPI_FLASH_B2_SIZE) & ~(0x3ff));
 	}
 	qspi_common.init = true;
 
