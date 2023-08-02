f09946635a1c5fa06e1db95f5feea8806b3ff187
Hubert Badocha <hubert.badocha@phoenix-rtos.com>
2023-08-02 14:23:04 +0200

    zynq7000-flash: qspi_transfer logic rewrite
    Rewrite qspi_transfer logic.
    Add workaround for bug in zynq hardware.
    
    JIRA: RTOS-537

--- storage/zynq7000-flash/qspi.h
+++ storage/zynq7000-flash/qspi.h
@@ -18,6 +18,10 @@
 #include <stdint.h>
 #include <time.h>
 
+
+/* NOTE: All synchronization and exclusion must be done externally.  */
+
+
 /* Transmit and receive number of data defines in 'size'. If TX Buff is empty, the dummy words are sent,
  * otherwise txBuff data is sent, returns EOK on success or <0 on error */
 extern ssize_t qspi_transfer(const uint8_t *txBuff, uint8_t *rxBuff, size_t size, time_t timeout);
@@ -38,7 +42,7 @@ extern int qspi_deinit(void);
 /* Initialize controller:
  * - I/O mode and manual mode,
  * - little endian
- * - src clk = 200 MHz, qspi clk = 50 MHz
+ * - src clk = 200 MHz, qspi clk = 100 MHz or 50 MHz if Feedback Clock (QSPI_FCLK < 0) pin is not used.
  * returns EOK on success <0 on error */
 extern int qspi_init(void);
 
