0e4a673d586a2f5b7d0b8f08bbb376e27df8c497
Michał Woyke <michal.woyke@phoenix-rtos.com>
2024-01-15 15:56:51 +0100

    imxrt-flash: add custom flashsrv init option
    Allows to perform direct operations (read/write/sector erase) on
    specific device/partition from user code.
    
    JIRA:NIL-471

--- storage/imxrt-flash/imxrt-flashsrv.h
+++ storage/imxrt-flash/imxrt-flashsrv.h
@@ -35,16 +35,36 @@ typedef struct {
 
 
 typedef struct {
+	uint32_t size;
+	uint32_t psize;
+	uint32_t ssize;
+	uint32_t offs;
+} flashsrv_properties_t;
+
+
+typedef struct {
 	int err;
 
-	struct {
-		uint32_t size;
-		uint32_t psize;
-		uint32_t ssize;
-		uint32_t offs;
-	} properties;
+	flashsrv_properties_t properties;
 
 } __attribute__((packed)) flash_o_devctl_t;
+
+
+/*
+ * Device/partition operations
+ *
+ * Includes parameters and callbacks which allow to perform direct operations (read/write/sector erase) on specific
+ * device/partition. Callbacks are initialized by flashsrv. User should provide *fID* inside flashsrv_customIntInit()
+ * call. It should be passed as the first argument to callbacks.
+ */
+typedef struct {
+	uint8_t fID;
+
+	ssize_t (*read)(uint8_t fID, size_t offset, void *data, size_t size);
+	ssize_t (*write)(uint8_t fID, size_t offset, const void *data, size_t size);
+	int (*eraseSector)(uint8_t fID, uint32_t offs);
+	int (*getProperties)(uint8_t fID, flashsrv_properties_t *p);
+} flashsrv_partitionOps_t;
 
 
 #endif
