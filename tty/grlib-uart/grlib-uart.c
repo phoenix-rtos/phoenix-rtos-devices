407a22e3cde81adc507a939498f5f601f3583250
Łukasz Leczkowski <lukasz.leczkowski@phoenix-rtos.com>
2024-08-01 15:09:47 +0200

    rename sparcv8leon3 to sparcv8leon
    JIRA: RTOS-883

--- tty/grlib-uart/grlib-uart.c
+++ tty/grlib-uart/grlib-uart.c
@@ -1,7 +1,7 @@
 /*
  * Phoenix-RTOS
  *
- * sparcv8leon3-grlib UART driver
+ * sparcv8leon-grlib UART driver
  *
  * Copyright 2023 Phoenix Systems
  * Author: Lukasz Leczkowski
@@ -34,8 +34,8 @@
 
 #include <phoenix/ioctl.h>
 
-#if defined(__TARGET_SPARCV8LEON3)
-#include <phoenix/arch/sparcv8leon3/sparcv8leon3.h>
+#if defined(__TARGET_SPARCV8LEON)
+#include <phoenix/arch/sparcv8leon/sparcv8leon.h>
 #elif defined(__TARGET_RISCV64)
 #include <phoenix/arch/riscv64/riscv64.h>
 #endif
