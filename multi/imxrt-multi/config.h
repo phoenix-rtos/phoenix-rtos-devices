b6f417fe6dffd0cbbdabdb9bdc9676e9c575bfe5
Jacek Maksymowicz <jacek.maksymowicz@phoenix-rtos.com>
2024-09-26 18:45:13 +0200

    imxrt-multi: Allow UART_CONSOLE_USER to be defined empty
    Rename RTT_CHANNEL_* to RTT_*
    
    JIRA: RTOS-754

--- multi/imxrt-multi/config.h
+++ multi/imxrt-multi/config.h
@@ -754,7 +754,7 @@
 
 /* clang-format on */
 
-#if !defined(UART_CONSOLE) && !defined(RTT_CHANNEL_CONSOLE)
+#ifndef UART_CONSOLE
 #if defined(__CPU_IMXRT105X)
 #define UART_CONSOLE 1
 #elif defined(__CPU_IMXRT106X)
@@ -766,54 +766,63 @@
 #endif
 #endif
 
-#if defined(UART_CONSOLE)
-#if ISEMPTY(UART_CONSOLE)
-#error "UART_CONSOLE must not be empty"
-#elif UART_CONSOLE <= 0
+#if !ISEMPTY(UART_CONSOLE)
+#if UART_CONSOLE <= 0
 #error "Invalid value for UART_CONSOLE"
 #endif
+#endif
+
+#if defined(UART_CONSOLE_USER)
+#if !ISEMPTY(UART_CONSOLE_USER)
+#if (UART_CONSOLE_USER <= 0)
+#error "Invalid value for UART_CONSOLE_USER"
+#endif
+#endif
+#else
+#define UART_CONSOLE_USER UART_CONSOLE
 #endif
 
 
 /* RTT */
 
-#ifndef RTT_CHANNEL0
-#define RTT_CHANNEL0 0
-#elif !ISBOOLEAN(RTT_CHANNEL0)
-#error "RTT_CHANNEL0 must have a value of 0, 1, or be undefined"
+#ifndef RTT_0
+#define RTT_0 0
+#elif !ISBOOLEAN(RTT_0)
+#error "RTT_0 must have a value of 0, 1, or be undefined"
 #endif
 
-#ifndef RTT_CHANNEL1
-#define RTT_CHANNEL1 0
-#elif !ISBOOLEAN(RTT_CHANNEL1)
-#error "RTT_CHANNEL1 must have a value of 0, 1, or be undefined"
+#ifndef RTT_1
+#define RTT_1 0
+#elif !ISBOOLEAN(RTT_1)
+#error "RTT_1 must have a value of 0, 1, or be undefined"
 #endif
 
-#ifndef RTT_CHANNEL0_BLOCKING
-#define RTT_CHANNEL0_BLOCKING 0
-#elif !ISBOOLEAN(RTT_CHANNEL0_BLOCKING)
-#error "RTT_CHANNEL0_BLOCKING must have a value of 0, 1, or be undefined"
+#ifndef RTT_0_BLOCKING
+#define RTT_0_BLOCKING 0
+#elif !ISBOOLEAN(RTT_0_BLOCKING)
+#error "RTT_0_BLOCKING must have a value of 0, 1, or be undefined"
 #endif
 
-#ifndef RTT_CHANNEL1_BLOCKING
-#define RTT_CHANNEL1_BLOCKING 0
-#elif !ISBOOLEAN(RTT_CHANNEL1_BLOCKING)
-#error "RTT_CHANNEL1_BLOCKING must have a value of 0, 1, or be undefined"
+#ifndef RTT_1_BLOCKING
+#define RTT_1_BLOCKING 0
+#elif !ISBOOLEAN(RTT_1_BLOCKING)
+#error "RTT_1_BLOCKING must have a value of 0, 1, or be undefined"
 #endif
 
 
-#if defined(UART_CONSOLE) && defined(RTT_CHANNEL_CONSOLE)
-#error "Console on UART and RTT not supported"
-#elif defined(RTT_CHANNEL_CONSOLE)
-#if ISEMPTY(RTT_CHANNEL_CONSOLE)
-#error "RTT_CHANNEL_CONSOLE must not be empty"
-#elif RTT_CHANNEL_CONSOLE < 0
-#error "Invalid value for RTT_CHANNEL_CONSOLE"
+#ifndef RTT_CONSOLE_USER
+#define RTT_CONSOLE_USER
+#elif !ISEMPTY(RTT_CONSOLE_USER)
+#if RTT_CONSOLE_USER < 0
+#error "Invalid value for RTT_CONSOLE_USER"
+#endif
 #endif
 
-#define ONLY_RTT_CONSOLE
+#if !ISEMPTY(UART_CONSOLE_USER) && !ISEMPTY(RTT_CONSOLE_USER)
+#error "Console on both UART and RTT not supported"
+#elif ISEMPTY(UART_CONSOLE_USER) && ISEMPTY(RTT_CONSOLE_USER)
+#error "Console must be either on UART or RTT"
 #endif
-
 
 /* SPI */
 
