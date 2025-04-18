5caafce9229215b2f5393c7e17eebec758fd05ac
Adam Greloch <adam.greloch@phoenix-rtos.com>
2025-04-18 14:16:27 +0200

    usbacm: adapt to events API
    JIRA: RTOS-1024
    (cherry picked from commit 894ed81c1b38704bfe962c6e92d7832433c63899)

--- tty/usbacm/usbacm.c
+++ tty/usbacm/usbacm.c
@@ -639,7 +639,7 @@ static void usbacm_msgthr(void *arg)
 }
 
 
-static int usbacm_handleInsertion(usb_driver_t *drv, usb_devinfo_t *insertion)
+static int usbacm_handleInsertion(usb_driver_t *drv, usb_devinfo_t *insertion, usb_event_insertion_t *event)
 {
 	usbacm_dev_t *dev;
 	const usb_modeswitch_t *mode;
@@ -727,6 +727,10 @@ static int usbacm_handleInsertion(usb_driver_t *drv, usb_devinfo_t *insertion)
 	}
 
 	fprintf(stdout, "usbacm: New device: %s\n", dev->path);
+
+	event->deviceCreated = true;
+	event->dev = oid;
+	strncpy(event->devPath, dev->path, sizeof(event->devPath));
 
 	return 0;
 }
