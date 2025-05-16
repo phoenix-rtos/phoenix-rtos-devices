0b8d944363e6ba46a78841bbca3cb63438be456c
Dariusz Sabała <dariusz.sabala@phoenix-rtos.com>
2025-05-16 13:21:57 +0200

    We are probably close to link

--- pcie/zynqmp/pcie.c
+++ pcie/zynqmp/pcie.c
@@ -40,7 +40,7 @@
 #define PCIREG_ADDRESS 0xfd480000
 
 #define CFG_SIZE 0x10000000
-#define CFG_ADDRESS 0x8000000000
+#define CFG_ADDRESS 0xE0000000
 
 #define GIC_PCIE_INTX_IRQ 148
 
@@ -81,51 +81,6 @@
 #define upper_32_bits(n) ((uint32_t)(((n) >> 16) >> 16))
 
 #define lower_32_bits(n) ((uint32_t)((n) & 0xffffffff))
-
-#define PCIE_ATTRIB_ATTR_4_OFFSET     0X0010
-#define PCIE_ATTRIB_ATTR_8_OFFSET     0X0020
-#define PCIE_ATTRIB_ATTR_7_OFFSET     0X001C
-#define PCIE_ATTRIB_ATTR_9_OFFSET     0X0024
-#define PCIE_ATTRIB_ATTR_10_OFFSET    0X0028
-#define PCIE_ATTRIB_ATTR_11_OFFSET    0X002C
-#define PCIE_ATTRIB_ATTR_12_OFFSET    0X0030
-#define PCIE_ATTRIB_ATTR_13_OFFSET    0X0034
-#define PCIE_ATTRIB_ATTR_14_OFFSET    0X0038
-#define PCIE_ATTRIB_ATTR_15_OFFSET    0X003C
-#define PCIE_ATTRIB_ATTR_16_OFFSET    0X0040
-#define PCIE_ATTRIB_ATTR_17_OFFSET    0X0044
-#define PCIE_ATTRIB_ATTR_18_OFFSET    0X0048
-#define PCIE_ATTRIB_ATTR_24_OFFSET    0X0060
-#define PCIE_ATTRIB_ATTR_25_OFFSET    0X0064
-#define PCIE_ATTRIB_ATTR_25_OFFSET    0X0064
-#define PCIE_ATTRIB_ATTR_27_OFFSET    0X006C
-#define PCIE_ATTRIB_ATTR_34_OFFSET    0X0088
-#define PCIE_ATTRIB_ATTR_35_OFFSET    0X008C
-#define PCIE_ATTRIB_ATTR_37_OFFSET    0X0094
-#define PCIE_ATTRIB_ATTR_41_OFFSET    0X00A4
-#define PCIE_ATTRIB_ATTR_43_OFFSET    0X00AC
-#define PCIE_ATTRIB_ATTR_44_OFFSET    0X00B0
-#define PCIE_ATTRIB_ATTR_45_OFFSET    0X00B4
-#define PCIE_ATTRIB_ATTR_46_OFFSET    0X00B8
-#define PCIE_ATTRIB_ATTR_47_OFFSET    0X00BC
-#define PCIE_ATTRIB_ATTR_48_OFFSET    0X00C0
-#define PCIE_ATTRIB_ATTR_50_OFFSET    0X00C8
-#define PCIE_ATTRIB_ATTR_53_OFFSET    0X00D4
-#define PCIE_ATTRIB_ATTR_79_OFFSET    0X013C
-#define PCIE_ATTRIB_ATTR_89_OFFSET    0X0164
-#define PCIE_ATTRIB_ATTR_93_OFFSET    0X0174
-#define PCIE_ATTRIB_ATTR_97_OFFSET    0X0184
-#define PCIE_ATTRIB_ATTR_100_OFFSET   0X0190
-#define PCIE_ATTRIB_ATTR_101_OFFSET   0X0194
-#define PCIE_ATTRIB_ATTR_105_OFFSET   0X01A4
-#define PCIE_ATTRIB_ATTR_106_OFFSET   0X01A8
-#define PCIE_ATTRIB_ATTR_107_OFFSET   0X01AC
-#define PCIE_ATTRIB_ATTR_108_OFFSET   0X01B0
-#define PCIE_ATTRIB_ATTR_109_OFFSET   0X01B4
-#define PCIE_ATTRIB_CB_OFFSET         0X031C
-#define PCIE_ATTRIB_ID_OFFSET         0X0200
-#define PCIE_ATTRIB_REV_ID_OFFSET     0X0208
-#define PCIE_ATTRIB_SUBSYS_ID_OFFSET  0X0204
 
 /* Data structure used by the driver */
 static struct {
@@ -211,65 +166,6 @@ static int pcitest_configPsGtr(void)
 	return 0;
 }
 
-static int pcietest_releasePcieCfgRst(void)
-{
-	platformctl_t ctl = {
-		.action = pctl_set,
-		.type = pctl_devreset,
-		.devreset.dev = pctl_devreset_fpd_pcie_ctrl,
-		.devreset.state = 0,
-	};
-	return platformctl(&ctl);
-}
-
-static int pcietest_releaseGtRst(void)
-{
-	platformctl_t ctl = {
-		.action = pctl_set,
-		.type = pctl_devreset,
-		.devreset.dev = pctl_devreset_fpd_gt,
-		.devreset.state = 0,
-	};
-	return platformctl(&ctl);
-}
-
-static int pcietest_releasePcieBridgeRst(void)
-{
-	platformctl_t ctl = {
-		.action = pctl_set,
-		.type = pctl_devreset,
-		.devreset.dev = pctl_devreset_fpd_pcie_bridge,
-		.devreset.state = 0,
-	};
-	return platformctl(&ctl);
-}
-
-static int pcietest_releasePcieRst(void)
-{
-	platformctl_t ctl = {
-		.action = pctl_set,
-		.type = pctl_devreset,
-		.devreset.dev = pctl_devreset_fpd_pcie_cfg,
-		.devreset.state = 0,
-	};
-	return platformctl(&ctl);
-}
-
-static int pcietest_initPcieClock(void)
-{
-	platformctl_t ctl = {
-		.action = pctl_set,
-		.type = pctl_devclock,
-		.devclock.dev = pctl_devclock_fpd_pcie,
-		.devclock.src = 0,  /* source: IOPLL_TO_FPD (500 MHz) */
-		.devclock.div0 = 2, /* divide by 2 = 250 MHz */
-		.devclock.div1 = 0,
-		.devclock.active = 0x1,
-	};
-
-	return platformctl(&ctl);
-}
-
 static int pcietest_pciIntxIrqHandler(unsigned int id, void *arg)
 {
 	return 0;
@@ -320,60 +216,6 @@ static int pcietest_initPcie(void)
 	if (interrupt(GIC_PCIE_INTX_IRQ, pcietest_pciIntxIrqHandler, &pcie, pcie.cond, &pcie.inth) != 0) {
 		printf("pcietest: failed to register interrupt handler\n");
 		return -1;
-	}
-
-	printf("pcitest: setup pcie attributes...\n");
-	/* Lets to it! */
-    writeRegMsk(pcie.pcireg, PCIE_ATTRIB_ATTR_25_OFFSET, 0x00000200U, 0x00000200U);
-	writeRegMsk(pcie.pcireg, PCIE_ATTRIB_ATTR_7_OFFSET, 0x0000FFFFU, 0x00000000U);
-    writeRegMsk(pcie.pcireg, PCIE_ATTRIB_ATTR_8_OFFSET, 0x0000FFFFU, 0x00000000U);
-    writeRegMsk(pcie.pcireg, PCIE_ATTRIB_ATTR_9_OFFSET, 0x0000FFFFU, 0x00000000U);
-    writeRegMsk(pcie.pcireg, PCIE_ATTRIB_ATTR_10_OFFSET, 0x0000FFFFU, 0x00000000U);
-    writeRegMsk(pcie.pcireg, PCIE_ATTRIB_ATTR_11_OFFSET, 0x0000FFFFU, 0x0000FFFFU);
-    writeRegMsk(pcie.pcireg, PCIE_ATTRIB_ATTR_12_OFFSET, 0x0000FFFFU, 0x000000FFU);
-    writeRegMsk(pcie.pcireg, PCIE_ATTRIB_ATTR_13_OFFSET, 0x0000FFFFU, 0x00000000U);
-    writeRegMsk(pcie.pcireg, PCIE_ATTRIB_ATTR_14_OFFSET, 0x0000FFFFU, 0x0000FFFFU);
-    writeRegMsk(pcie.pcireg, PCIE_ATTRIB_ATTR_15_OFFSET, 0x0000FFFFU, 0x0000FFF0U);
-    writeRegMsk(pcie.pcireg, PCIE_ATTRIB_ATTR_16_OFFSET, 0x0000FFFFU, 0x0000FFF0U);
-    writeRegMsk(pcie.pcireg, PCIE_ATTRIB_ATTR_17_OFFSET, 0x0000FFFFU, 0x0000FFF1U);
-    writeRegMsk(pcie.pcireg, PCIE_ATTRIB_ATTR_18_OFFSET, 0x0000FFFFU, 0x0000FFF1U);
-    writeRegMsk(pcie.pcireg, PCIE_ATTRIB_ATTR_27_OFFSET, 0x00000738U, 0x00000100U);
-    writeRegMsk(pcie.pcireg, PCIE_ATTRIB_ATTR_50_OFFSET, 0x0000FFF0U, 0x00000040U);
-    writeRegMsk(pcie.pcireg, PCIE_ATTRIB_ATTR_105_OFFSET, 0x000007FFU, 0x000000CDU);
-    writeRegMsk(pcie.pcireg, PCIE_ATTRIB_ATTR_106_OFFSET, 0x00003FFFU, 0x00000624U);
-    writeRegMsk(pcie.pcireg, PCIE_ATTRIB_ATTR_107_OFFSET, 0x000007FFU, 0x00000018U);
-    writeRegMsk(pcie.pcireg, PCIE_ATTRIB_ATTR_108_OFFSET, 0x000007FFU, 0x000000B5U);
-    writeRegMsk(pcie.pcireg, PCIE_ATTRIB_ATTR_109_OFFSET, 0x0000FFFFU, 0x00007E20U);
-    writeRegMsk(pcie.pcireg, PCIE_ATTRIB_ATTR_34_OFFSET, 0x000000FFU, 0x00000001U);
-    writeRegMsk(pcie.pcireg, PCIE_ATTRIB_ATTR_53_OFFSET, 0x000000FFU, 0x00000060U);
-    writeRegMsk(pcie.pcireg, PCIE_ATTRIB_ATTR_41_OFFSET, 0x000003FFU, 0x00000000U);
-	writeRegMsk(pcie.pcireg, PCIE_ATTRIB_ATTR_97_OFFSET, 0x00000FFFU, 0x00000041U);
-	writeRegMsk(pcie.pcireg, PCIE_ATTRIB_ATTR_100_OFFSET, 0x00000040U, 0x00000000U);
-    writeRegMsk(pcie.pcireg, PCIE_ATTRIB_ATTR_101_OFFSET, 0x0000FFE2U, 0x0000FFE2U);
-    writeRegMsk(pcie.pcireg, PCIE_ATTRIB_ATTR_37_OFFSET, 0x00007E00U, 0x00004A00U);
-    writeRegMsk(pcie.pcireg, PCIE_ATTRIB_ATTR_93_OFFSET, 0x0000FFFFU, 0x00009000U);
-    writeRegMsk(pcie.pcireg, PCIE_ATTRIB_ID_OFFSET, 0xFFFFFFFFU, 0x10EED021U);
-    writeRegMsk(pcie.pcireg, PCIE_ATTRIB_SUBSYS_ID_OFFSET, 0xFFFFFFFFU, 0x10EE0007U);
-    writeRegMsk(pcie.pcireg, PCIE_ATTRIB_REV_ID_OFFSET, 0x000000FFU, 0x00000000U);
-    writeRegMsk(pcie.pcireg, PCIE_ATTRIB_ATTR_24_OFFSET, 0x0000FFFFU, 0x00000400U);
-    writeRegMsk(pcie.pcireg, PCIE_ATTRIB_ATTR_25_OFFSET, 0x000001FFU, 0x00000006U);
-    writeRegMsk(pcie.pcireg, PCIE_ATTRIB_ATTR_4_OFFSET, 0x00001000U, 0x00000000U);
-    writeRegMsk(pcie.pcireg, PCIE_ATTRIB_ATTR_89_OFFSET, 0x00001FFEU, 0x00000000U);
-    writeRegMsk(pcie.pcireg, PCIE_ATTRIB_ATTR_79_OFFSET, 0x00000020U, 0x00000020U);
-    writeRegMsk(pcie.pcireg, PCIE_ATTRIB_ATTR_43_OFFSET, 0x00000100U, 0x00000000U);
-    writeRegMsk(pcie.pcireg, PCIE_ATTRIB_ATTR_48_OFFSET, 0x000007FFU, 0x00000000U);
-    writeRegMsk(pcie.pcireg, PCIE_ATTRIB_ATTR_46_OFFSET, 0x0000FFFFU, 0x00000000U);
-    writeRegMsk(pcie.pcireg, PCIE_ATTRIB_ATTR_47_OFFSET, 0x00001FFFU, 0x00000000U);
-    writeRegMsk(pcie.pcireg, PCIE_ATTRIB_ATTR_44_OFFSET, 0x0000FFFFU, 0x00000000U);
-    writeRegMsk(pcie.pcireg, PCIE_ATTRIB_ATTR_45_OFFSET, 0x0000FFF8U, 0x00000000U);
-    writeRegMsk(pcie.pcireg, PCIE_ATTRIB_CB_OFFSET, 0x00000002U, 0x00000000U);
-    writeRegMsk(pcie.pcireg, PCIE_ATTRIB_ATTR_35_OFFSET, 0x0000B000U, 0x00008000U);
-
-	/* Release reset from required subsystems */
-	int ret = pcietest_releasePcieRst();
-	if (ret != 0) {
-		printf("pcietest: fail to release reset from PCI Express\n");
-		return ret;
 	}
 
 	usleep(1000);
@@ -441,11 +283,18 @@ static int pcietest_initPcie(void)
 	writeReg(pcie.breg, E_ECAM_BASE_LO, lower_32_bits(CFG_ADDRESS));
 	writeReg(pcie.breg, E_ECAM_BASE_HI, upper_32_bits(CFG_ADDRESS));
 
-	usleep(100000);
-
 	/* Check link status */
-	uint32_t pcie_link_status = readReg(pcie.pcireg, PS_LINKUP_OFFSET);
-	if (pcie_link_status & 0x1) {
+	bool pcie_link_up = false;
+	for (int retries = 0; retries < LINK_WAIT_MAX_RETRIES; retries++) {
+		uint32_t pcie_link_status = readReg(pcie.pcireg, PS_LINKUP_OFFSET);
+		if (pcie_link_status & 0x1) {
+			pcie_link_up = true;
+			break;
+		}
+		usleep(100000);
+	}
+
+	if (pcie_link_up) {
 		printf("pcietest: pcie link up\n");
 	}
 	else {
@@ -460,33 +309,10 @@ static int pcietest_initPcie(void)
 	return 0;
 }
 
-static int pcietest_init(void)
+int main(int argc, char **argv)
 {
 	int ret = 0;
 
-	/* Program PCI Express peripheral clock */
-	printf("pcietest: init PCI Express clock...\n");
-	ret = pcietest_initPcieClock();
-	if (ret != 0) {
-		printf("pcietest: fail to config PCI Express clock\n");
-		return ret;
-	}
-	printf("pcietest: release resets...\n");
-	ret = pcietest_releasePcieCfgRst();
-	if (ret != 0) {
-		printf("pcietest: fail to release reset from PCI Express Config\n");
-		return ret;
-	}
-	ret = pcietest_releasePcieBridgeRst();
-	if (ret != 0) {
-		printf("pcietest: fail to release reset from PCI Express Bridge\n");
-		return ret;
-	}
-	ret = pcietest_releaseGtRst();
-	if (ret != 0) {
-		printf("pcietest: fail to release reset from GT\n");
-		return ret;
-	}
 	/* Configure PS GTR transceivers */
 	printf("pcietest: configure PS GTR transceivers...\n");
 	ret = pcitest_configPsGtr();
@@ -504,14 +330,4 @@ static int pcietest_init(void)
 	}
 
 	return 0;
-}
-
-int main(int argc, char **argv)
-{
-	if (pcietest_init() < 0) {
-		fprintf(stderr, "pcietest: cannot initialize PCI Express\n");
-		return EXIT_FAILURE;
-	}
-
-	return EXIT_SUCCESS;
 }
