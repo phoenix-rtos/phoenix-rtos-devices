cde0f14416af61f95505c267cf7b0fab905d958a
Adam Greloch <adam.greloch@phoenix-rtos.com>
2025-06-06 12:53:17 +0200

    umass: access requests in FIFO order
    There is no clear benefit from LIFO processing
    
    JIRA: RTOS-970
    (cherry picked from commit 073b881951e19ac8ae391a719f4bf4efc30dded3)

--- storage/umass/umass.c
+++ storage/umass/umass.c
@@ -672,7 +672,12 @@ static void umass_poolthr(void *arg)
 		while (umass_common.rqueue == NULL) {
 			condWait(umass_common.rcond, umass_common.rlock, 0);
 		}
-		req = umass_common.rqueue->prev;
+
+		/*
+		 * FIFO - assumes LIST_ADD/REMOVE set the queue pointer to the
+		 * element added first
+		 */
+		req = umass_common.rqueue;
 		LIST_REMOVE(&umass_common.rqueue, req);
 
 		mutexUnlock(umass_common.rlock);
