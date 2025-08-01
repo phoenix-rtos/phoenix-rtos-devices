bec23317e0a004f9d533353dec6e04581d57a8a5
winderdoot <krzysztof.radzewicz@phoenix-rtos.com>
2025-08-01 10:19:35 +0200

    WIP stm32n6 cryp driver

---
+++ multi/stm32l4-multi/libmulti/libcryp.c
@@ -0,0 +1,355 @@
+/*
+ * Phoenix-RTOS
+ *
+ * Multidrv-lib: STM32N6 CRYP driver
+ *
+ * Copyright 2025 Phoenix Systems
+ * Author: Krzysztof Radzewicz
+ * %LICENSE%
+ */
+
+#include "../common.h"
+#include "libmulti/libcryp.h"
+#include <errno.h>
+
+/*
+NOTE: The tinyaes implementation will have to be changed,
+bcs CRYP excpects to initialize it's registers in a different order than AES on stm32nl4
+Keep the API mostly the same but change how it's called in tinyaes
+
+
+*/
+
+#define CR_IPRST      (31)
+#define CR_KMOD       (24)
+#define CR_NPBLB      (20)
+#define CR_ALGOMODE_H (19)
+#define CR_GCM_CCMPH  (16)
+#define CR_ENABLE     (15)
+#define CR_FFLUSH     (14)
+#define CR_KEYSIZE    (8)
+#define CR_DATATYPE   (6)
+#define CR_ALGOMODE_L (3)
+#define CR_ALGODIR    (2)
+
+#define SR_KEYVALID (7)
+#define SR_KERF     (6)
+#define SR_BUSY     (4)
+#define SR_OFFU     (3)
+#define SR_OFNE     (2)
+#define SR_IFNF     (1)
+#define SR_IFEM     (0)
+
+#define CRYP_BASE ((void *)0x54020800)
+
+enum {
+	cryp_cr,
+	cryp_sr,
+	cryp_dinr,
+	cryp_doutr,
+	cryp_dmacr,
+	cryp_imscr,
+	cryp_risr,
+	cryp_misr,
+	cryp_k0lr,
+	cryp_k0rr,
+	cryp_k1lr,
+	cryp_k1rr,
+	cryp_k2lr,
+	cryp_k2rr,
+	cryp_k3lr,
+	cryp_k3rr,
+	cryp_iv0lr,
+	cryp_iv0rr,
+	cryp_iv1lr,
+	cryp_iv1rr,
+	cryp_csgcmccm0r,
+	cryp_csgcmccm1r,
+	cryp_csgcmccm2r,
+	cryp_csgcmccm3r,
+	cryp_csgcmccm4r,
+	cryp_csgcmccm5r,
+	cryp_csgcmccm6r,
+	cryp_csgcmccm7r,
+	cryp_csgcm0r,
+	cryp_csgcm1r,
+	cryp_csgcm2r,
+	cryp_csgcm3r,
+	cryp_csgcm4r,
+	cryp_csgcm5r,
+	cryp_csgcm6r,
+	cryp_csgcm7r,
+};
+
+
+static struct {
+	volatile unsigned int *base;
+} common;
+
+
+// Can only be enabled when KEYVALID is set
+inline void libcryp_enable(void)
+{
+	dataBarier();
+	*(common.base + cryp_cr) |= (0x1 << CR_ENABLE);
+	dataBarier();
+}
+
+
+inline void libcryp_disable(void)
+{
+	dataBarier();
+	*(common.base + cryp_cr) &= ~(0x1 << CR_ENABLE);
+	dataBarier();
+}
+
+
+static inline void waitBusy()
+{
+	while ((*(common.base + cryp_sr) & (0x1 << SR_BUSY)) != 0)
+		;
+}
+
+
+/* Stores veclen bytes from vector, lowest address to highest starting at reg register.
+ * veclen has to be a multiple of 4
+ */
+static void storeVector(int reg, const unsigned char *vector, int veclen)
+{
+	int i;
+	unsigned int tmp;
+	volatile unsigned int *addr = common.base + reg;
+
+	for (i = 0; i * 4 < veclen; i++) {
+		tmp = *vector++ << 24;
+		tmp |= *vector++ << 16;
+		tmp |= *vector++ << 8;
+		tmp |= *vector++;
+		*addr++ = tmp;
+	}
+}
+
+/* Retrieves veclen bytes from lowest address to highest starting at reg register.
+ * veclen has to be a multiple of 4
+ */
+static void retrieveVector(int reg, unsigned char *vector, int veclen)
+{
+	int i;
+	unsigned int tmp;
+	volatile unsigned int *addr = common.base + reg;
+
+	for (i = 0; i * 4 < veclen; i++) {
+		tmp = *addr++;
+		*vector++ = tmp >> 24;
+		*vector++ = tmp >> 16;
+		*vector++ = tmp >> 8;
+		*vector++ = tmp;
+	}
+}
+
+
+int libcryp_setKey(const unsigned char *key, int keylen)
+{
+	unsigned int t;
+	waitBusy();
+
+	t = *(common.base + cryp_cr) & ~(0x3 << CR_KEYSIZE);
+
+	if (keylen == aes_128) {
+		*(common.base + cryp_cr) = t | (0x0 << CR_KEYSIZE);
+		storeVector(cryp_k2lr, key, 16);
+	}
+	else if (keylen == aes_192) {
+		*(common.base + cryp_cr) = t | (0x1 << CR_KEYSIZE);
+		storeVector(cryp_k1lr, key, 24);
+	}
+	else {
+		*(common.base + cryp_cr) = t | (0x2 << CR_KEYSIZE);
+		storeVector(cryp_k0lr, key, 32);
+	}
+
+	/* Check if key writing sequence succeded */
+	if ((*(common.base + cryp_sr) & (0x1 << SR_KEYVALID)) == 0) {
+		return -EINVAL;
+	}
+
+	libcryp_enable();
+	waitBusy();
+
+	return EOK;
+}
+
+
+void libcryp_getKey(unsigned char *key, int keylen)
+{
+	return -ENOSYS;
+}
+
+void libcryp_deriveDecryptionKey()
+{
+	unsigned int t;
+
+	libcryp_disable();
+	waitBusy();
+
+	/* Flush I/O FIFO */
+	*(common.base + cryp_cr) |= (0x1 << CR_FFLUSH);
+
+	/* Clear ALGOMODE and ALGODIR to set them later */
+	t = *(common.base + cryp_cr) & ~((0x1 << CR_ALGOMODE_H) | (0x7 << CR_ALGOMODE_L) | (0x1 << CR_ALGODIR));
+
+	/* Set key derivation mode */
+	t |= (0x1 << CR_ALGOMODE_H) | (0x3 << CR_ALGOMODE_L);
+	*(common.base + cryp_cr) = t;
+
+	/* Disable data swapping */
+	*(common.base + cryp_cr) &= ~(0x3 << CR_DATATYPE);
+}
+
+
+void libcryp_setIv(const unsigned char *iv)
+{
+	storeVector(cryp_iv0lr, iv, 16);
+}
+
+
+void libcryp_getIv(unsigned char *iv)
+{
+	retrieveVector(cryp_iv0lr, iv, 16);
+}
+
+
+void libcryp_deriveDecryptionKey(void)
+{
+	return -ENOSYS;
+}
+
+// Very work in progressssssssss
+int tmp(unsigned char *key, unsigned char *iv, const unsigned char *in, unsigned char *out)
+{
+	unsigned int t;
+
+	libcryp_disable();
+
+	*(common.base + cryp_cr) |= (0x1 << CR_FFLUSH);
+
+	t = *(common.base + cryp_cr) & ~((0x1 << CR_ALGOMODE_H) | (0x7 << CR_ALGOMODE_L) | (0x1 << CR_ALGODIR));
+	t |= (0x5 << CR_ALGOMODE_L);
+	*(common.base + cryp_cr) = t;
+	*(common.base + cryp_cr) &= ~(0x3 << CR_DATATYPE);
+
+	t = *(common.base + cryp_cr) & ~(0x3 << CR_KEYSIZE);
+	*(common.base + cryp_cr) = t | (0x0 << CR_KEYSIZE);
+
+	*(common.base + cryp_cr) &= ~(0x3 << CR_KMOD);
+
+	libcryp_setIv(iv);
+
+	storeVector(cryp_k2lr, key, 16);
+
+	if ((*(common.base + cryp_sr) & (0x1 << SR_KEYVALID)) == 0) {
+		return -EINVAL;
+	}
+
+	libcryp_enable();
+
+	libcryp_processBlock(in, out);
+	// libcryp_setIv(out);
+
+	libcryp_processBlock(in + 16, out + 16);
+
+	libcryp_unprepare();
+	return 0;
+}
+
+
+void libcryp_prepare(int mode, int dir)
+{
+	unsigned int t;
+
+	waitBusy();
+
+	/* Flush I/O FIFO */
+	*(common.base + cryp_cr) |= (0x1 << CR_FFLUSH);
+
+	/* Clear ALGOMODE and ALGODIR to set them later */
+	t = *(common.base + cryp_cr) & ~((0x1 << CR_ALGOMODE_H) | (0x7 << CR_ALGOMODE_L) | (0x1 << CR_ALGODIR));
+	switch (mode) {
+		case aes_ecb:
+			t |= (0x4 << CR_ALGOMODE_L);
+			break;
+		case aes_cbc:
+			t |= (0x5 << CR_ALGOMODE_L);
+			break;
+		case aes_ctr:
+			t |= (0x6 << CR_ALGOMODE_L);
+			break;
+	}
+	if (dir == aes_decrypt) {
+		t |= (0x1 << CR_ALGODIR);
+	}
+	*(common.base + cryp_cr) = t;
+
+	/* Disable data swapping */
+	*(common.base + cryp_cr) &= ~(0x3 << CR_DATATYPE);
+}
+
+
+void libcryp_unprepare(void)
+{
+	waitBusy();
+	libcryp_disable();
+}
+
+
+/* Block must be 16 bytes long */
+void libcryp_processBlock(const unsigned char *in, unsigned char *out)
+{
+	int i;
+	unsigned int tmp;
+
+	// It should be enabled before the first block and disabled after the last one
+	// libcryp_enable();
+
+	/* Wait untill input FIFO not full */
+	while ((*(common.base + cryp_sr) & (0x1 << SR_IFNF)) == 0)
+		;
+
+	/* Treating incoming bytes as msb first and storing them as big endian */
+	for (i = 0; i < 4; i++) {
+		tmp = *in++ << 24;
+		tmp |= *in++ << 16;
+		tmp |= *in++ << 8;
+		tmp |= *in++;
+		*(common.base + cryp_dinr) = tmp;
+	}
+
+	/* Wait untill output FIFO not empty */
+	while ((*(common.base + cryp_sr) & (0x1 << SR_OFNE)) == 0)
+		;
+
+	for (i = 0; i < 4; i++) {
+		tmp = *(common.base + cryp_doutr);
+		*out = tmp;
+		*out++ = tmp >> 8;
+		*out++ = tmp >> 16;
+		*out++ = tmp >> 24;
+	}
+}
+
+
+int libcryp_init(void)
+{
+	common.base = CRYP_BASE;
+
+	devClk(pctl_cryp, 1);
+	libcryp_disable();
+
+	/* Probably unnecessary */
+	waitBusy();
+
+	/* Set normal key mode - don't share with SAES */
+	*(common.base + cryp_cr) &= ~(0x3 << CR_KMOD);
+
+	return 0;
+}
