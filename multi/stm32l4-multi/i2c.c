c31bae5fc69a3c820586f0392b3fd036a7b05a94
Jacek Maksymowicz <jacek.maksymowicz@phoenix-rtos.com>
2025-08-05 14:16:51 +0200

    stm32l4-multi: adjust code to pass array bounds checker
    The fix also reduces binary size if a particular driver is not used.
    
    JIRA: RTOS-1068

--- multi/stm32l4-multi/i2c.c
+++ multi/stm32l4-multi/i2c.c
@@ -26,34 +26,33 @@
 #include "i2c.h"
 #include "rcc.h"
 
-
+#define MAX_I2C i2c4
 
 #define I2C1_POS 0
 #define I2C2_POS (I2C1_POS + I2C1)
 #define I2C3_POS (I2C2_POS + I2C2)
 #define I2C4_POS (I2C3_POS + I2C3)
 
+#define N_I2C_ACTIVE (I2C1 + I2C2 + I2C3 + I2C4)
 
-libi2c_ctx_t i2c_ctx[I2C1 + I2C2 + I2C3 + I2C4];
-handle_t i2c_lock[I2C1 + I2C2 + I2C3 + I2C4];
-
-
-static const int i2cConfig[] = { I2C1, I2C2, I2C3, I2C4 };
+libi2c_ctx_t i2c_ctx[N_I2C_ACTIVE];
+handle_t i2c_lock[N_I2C_ACTIVE];
 
 
-static const int i2cPos[] = { I2C1_POS, I2C2_POS, I2C3_POS, I2C4_POS };
+static const int i2cConfig[MAX_I2C + 1] = { I2C1, I2C2, I2C3, I2C4 };
 
+
+static const int i2cPos[MAX_I2C + 1] = { I2C1_POS, I2C2_POS, I2C3_POS, I2C4_POS };
 
 
 ssize_t i2c_read(int i2c, unsigned char addr, void *buff, size_t len)
 {
-	ssize_t ret;
-
-	if (i2c < i2c1 || i2c > i2c4 || !i2cConfig[i2c])
+	if ((N_I2C_ACTIVE == 0) || (i2c < i2c1) || (i2c > MAX_I2C) || (i2cConfig[i2c] == 0)) {
 		return -1;
+	}
 
 	mutexLock(i2c_lock[i2cPos[i2c]]);
-	ret = libi2c_read(&i2c_ctx[i2cPos[i2c]], addr, buff, len);
+	ssize_t ret = libi2c_read(&i2c_ctx[i2cPos[i2c]], addr, buff, len);
 	mutexUnlock(i2c_lock[i2cPos[i2c]]);
 
 	return ret;
@@ -62,13 +61,12 @@ ssize_t i2c_read(int i2c, unsigned char addr, void *buff, size_t len)
 
 ssize_t i2c_readReg(int i2c, unsigned char addr, unsigned char reg, void *buff, size_t len)
 {
-	ssize_t ret;
-
-	if (i2c < i2c1 || i2c > i2c4 || !i2cConfig[i2c])
-		return -1;
+	if ((N_I2C_ACTIVE == 0) || (i2c < i2c1) || (i2c > MAX_I2C) || (i2cConfig[i2c] == 0)) {
+		return -EINVAL;
+	}
 
 	mutexLock(i2c_lock[i2cPos[i2c]]);
-	ret = libi2c_readReg(&i2c_ctx[i2cPos[i2c]], addr, reg, buff, len);
+	ssize_t ret = libi2c_readReg(&i2c_ctx[i2cPos[i2c]], addr, reg, buff, len);
 	mutexUnlock(i2c_lock[i2cPos[i2c]]);
 
 	return ret;
@@ -77,13 +75,12 @@ ssize_t i2c_readReg(int i2c, unsigned char addr, unsigned char reg, void *buff, 
 
 ssize_t i2c_write(int i2c, unsigned char addr, const void *buff, size_t len)
 {
-	ssize_t ret;
-
-	if (i2c < i2c1 || i2c > i2c4 || !i2cConfig[i2c])
-		return -1;
+	if ((N_I2C_ACTIVE == 0) || (i2c < i2c1) || (i2c > MAX_I2C) || (i2cConfig[i2c] == 0)) {
+		return -EINVAL;
+	}
 
 	mutexLock(i2c_lock[i2cPos[i2c]]);
-	ret = libi2c_write(&i2c_ctx[i2cPos[i2c]], addr, buff, len);
+	ssize_t ret = libi2c_write(&i2c_ctx[i2cPos[i2c]], addr, buff, len);
 	mutexUnlock(i2c_lock[i2cPos[i2c]]);
 
 	return ret;
@@ -92,13 +89,12 @@ ssize_t i2c_write(int i2c, unsigned char addr, const void *buff, size_t len)
 
 ssize_t i2c_writeReg(int i2c, unsigned char addr, unsigned char reg, const void *buff, size_t len)
 {
-	ssize_t ret;
-
-	if (i2c < i2c1 || i2c > i2c4 || !i2cConfig[i2c])
-		return -1;
+	if ((N_I2C_ACTIVE == 0) || (i2c < i2c1) || (i2c > MAX_I2C) || (i2cConfig[i2c] == 0)) {
+		return -EINVAL;
+	}
 
 	mutexLock(i2c_lock[i2cPos[i2c]]);
-	ret = libi2c_writeReg(&i2c_ctx[i2cPos[i2c]], addr, reg, buff, len);
+	ssize_t ret = libi2c_writeReg(&i2c_ctx[i2cPos[i2c]], addr, reg, buff, len);
 	mutexUnlock(i2c_lock[i2cPos[i2c]]);
 
 	return ret;
@@ -108,10 +104,14 @@ ssize_t i2c_writeReg(int i2c, unsigned char addr, unsigned char reg, const void 
 void i2c_init(void)
 {
 	int i2c;
+	if (N_I2C_ACTIVE == 0) {
+		return;
+	}
 
-	for (i2c = 0; i2c < 4; ++i2c) {
-		if (!i2cConfig[i2c])
+	for (i2c = i2c1; i2c <= MAX_I2C; ++i2c) {
+		if (!i2cConfig[i2c]) {
 			continue;
+		}
 
 		mutexCreate(&i2c_lock[i2cPos[i2c]]);
 		libi2c_init(&i2c_ctx[i2cPos[i2c]], i2c);
