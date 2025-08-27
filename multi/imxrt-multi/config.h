38bf08d748f8a70e9153599a7ffbb7cdb915179e
Jakub Klimek <jakub.klimek@phoenix-rtos.com>
2025-08-27 16:30:45 +0200

    imxrt-multi: introduce libcoredumpsrv
    Save target memory by including libcoredumpsrv in imxrt-multi instead of standalone executable.
    
    JIRA: RTOS-1054

--- multi/imxrt-multi/config.h
+++ multi/imxrt-multi/config.h
@@ -1436,6 +1436,11 @@
 #error "BUILTIN_POSIXSRV must have a value of 0, 1, or be undefined"
 #endif
 
+/* libcoredumpsrv */
+#ifndef BUILTIN_COREDUMPSRV
+#define BUILTIN_COREDUMPSRV 1
+#endif
+
 
 /* Pseudodev */
 
