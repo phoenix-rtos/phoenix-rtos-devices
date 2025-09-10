d4e83ada3a601c1b63416f63c627f3dda5754222
Aleksander Kaminski <aleksander.kaminski@phoenix-rtos.com>
2025-09-10 16:43:44 +0200

    Revert "tty/uart16550: rework interrupt handling"
    This reverts commit c23d1f456ea48c16cddfa8669cdb8e368d8ce968.

--- tty/uart16550/uart16550.h
+++ tty/uart16550/uart16550.h
@@ -15,6 +15,9 @@
 #ifndef _DEV_SERIAL_H_
 #define _DEV_SERIAL_H_
 
+#define SIZE_SERIALS      2
+#define SIZE_SERIAL_CHUNK 256
+
 
 /* UART registers */
 #define REG_RBR 0
@@ -35,21 +38,15 @@
 #define IMR_THRE 0x02
 #define IMR_DR   0x01
 
-#define IIR_IRQPEND   0x01
-#define IIR_CODE_MS   0x00 /* Modem Status */
-#define IIR_CODE_THRE 0x01 /* Transmitter Holding Register Empty */
-#define IIR_CODE_DR   0x02 /* Received Data Ready */
-#define IIR_CODE_LS   0x03 /* Receiver Line Status */
-#define IIR_CODE_RTO  0x06 /* Reception Timeout */
+#define IIR_IRQPEND 0x01
+#define IIR_THRE    0x02
+#define IIR_DR      0x04
 
 #define LCR_DLAB 0x80
 #define LCR_D8N1 0x03
 #define LCR_D8N2 0x07
 
 #define MCR_OUT2 0x08
-#define MCR_OUT1 0x04
-#define MCR_RTS  0x02
-#define MCR_DTR  0x01
 
 #define LSR_DR   0x01
 #define LSR_THRE 0x20
