ddd63c99b94ebaef7d29a2d39eb5c5d6a34ac17e
atlomak <andrzej.tlomak01@gmail.com>
2025-10-06 19:53:43 +0200

    wip

--- spacefibre/grspfi.c
+++ spacefibre/grspfi.c
@@ -29,6 +29,7 @@
 #include <posix/utils.h>
 #include <phoenix/gaisler/ambapp.h>
 #include <sys/msg.h>
+#include <inttypes.h>
 
 #include "grspfi.h"
 
@@ -43,6 +44,10 @@
 #define LOG(fmt, ...)       printf("spacefibre: " fmt "\n", ##__VA_ARGS__)
 #define LOG_ERROR(fmt, ...) fprintf(stderr, "spacefibre: " fmt "\n", ##__VA_ARGS__)
 /* clang-format on */
+
+#define PAGE_ALIGN(addr) (((addr_t)(addr) + _PAGE_SIZE - 1) & ~(_PAGE_SIZE - 1))
+
+#define SPFI_ADDR_MASK UINT64_C(0xffffffff) /* mask to check DMA desc. address overflow */
 
 #define SPFI_THREADS_NO 4
 #define SPFI_STACKSZ    4096
@@ -105,39 +110,25 @@ struct grspfi_regs {
 	/* DMA channel regs, stride 0x10 */
 	struct {
 		uint32_t ctrl;
+#define SPFI_DCCTRL_TS (1u << 3) /* TS - transmitter stop */
+#define SPFI_DCCTRL_RS (1u << 2) /* TE - receiver stop */
+#define SPFI_DCCTRL_TE (1u << 2) /* TE - transmitter enable */
+#define SPFI_DCCTRL_RE (1u << 1) /* TE - receiver enable */
 		uint32_t status;
 		uint32_t vc_map;
 		uint32_t irq_ctrl;
+#define SPFI_DCICTRL_RMI (1u << 3) /* RMAP irq enable */
+#define SPFI_DCICTRL_BEI (1u << 2) /* bus master error irq enable */
+#define SPFI_DCICTRL_TI  (1u << 1) /* transmit irq enable */
+#define SPFI_DCICTRL_RI  (1u << 0) /* receive irq enable */
 		uint32_t ext_status;
 	} dma_ch[8];
 };
 
+#define SPFI_RX_DESC_CNT 128
+#define SPFI_TX_DESC_CNT 64
 
-typedef struct {
-	volatile struct grspfi_regs *mmio;
-} spfi_dev_t;
-
-
-static struct {
-	volatile uint32_t *base;
-	unsigned int irq;
-	unsigned int active;
-} spfi_info[] = {
-	{ .active = SPFI0_ACTIVE },
-	{ .active = SPFI1_ACTIVE },
-	{ .active = SPFI2_ACTIVE },
-	{ .active = SPFI3_ACTIVE },
-	{ .active = SPFI4_ACTIVE },
-	{ .active = SPFI5_ACTIVE },
-};
-
-
-static struct {
-	oid_t spwOid;
-	spfi_dev_t dev[SPFI_CNT];
-	char spfiStack[SPFI_THREADS_NO][SPFI_STACKSZ];
-} spfi_common;
-
+#define SPFI_MAX_PACKET_LEN 1024
 
 #define SPFI_RX_DESC_DCRC    (1u << 31)   /* data crc error */
 #define SPFI_RX_DESC_HCRC    (1u << 30)   /* header crc error */
@@ -174,6 +165,54 @@ typedef struct {
 } spfi_txDesc_t;
 
 
+typedef struct {
+	volatile struct grspfi_regs *mmio;
+	uint8_t addr;
+	uint8_t txDescFree;
+
+	size_t sentDesc;
+	size_t lastTxDesc;
+	size_t nextRxDesc;
+
+	bool txWaited[SPFI_TX_DESC_CNT];
+	bool rxAcknowledged[SPFI_RX_DESC_CNT];
+
+	handle_t ctrlLock;
+	handle_t txLock;
+	handle_t txIrqLock;
+	handle_t rxLock;
+	handle_t rxConfLock;
+	handle_t cond;
+	handle_t rxAckCond;
+
+	volatile uint8_t (*txBuff)[SPFI_MAX_PACKET_LEN];
+	volatile uint8_t (*rxBuff)[SPFI_MAX_PACKET_LEN];
+	volatile spfi_txDesc_t *txDesc;
+	volatile spfi_rxDesc_t *rxDesc;
+} spfi_dev_t;
+
+
+static struct {
+	volatile uint32_t *base;
+	unsigned int irq;
+	unsigned int active;
+} spfi_info[] = {
+	{ .active = SPFI0_ACTIVE },
+	{ .active = SPFI1_ACTIVE },
+	{ .active = SPFI2_ACTIVE },
+	{ .active = SPFI3_ACTIVE },
+	{ .active = SPFI4_ACTIVE },
+	{ .active = SPFI5_ACTIVE },
+};
+
+
+static struct {
+	oid_t oid;
+	spfi_dev_t dev[SPFI_CNT];
+	char stack[SPFI_THREADS_NO][SPFI_STACKSZ];
+} spfi_common;
+
+
 static int spfi_set_vc(spfi_dev_t *dev, uint8_t dma, uint32_t vc)
 {
 	if (dma >= SPFI_DMA_CH) {
@@ -181,6 +220,59 @@ static int spfi_set_vc(spfi_dev_t *dev, uint8_t dma, uint32_t vc)
 	}
 	dev->mmio->dma_ch[dma].vc_map = vc;
 	return 0;
+}
+
+
+/* Configure RX DMA descriptors */
+static int spfi_rxConfigure(spfi_dev_t *dev, size_t *firstDesc, const size_t nPackets)
+{
+	if (nPackets > SPFI_RX_DESC_CNT) {
+		return -EINVAL;
+	}
+
+	(void)mutexLock2(dev->rxConfLock, dev->rxLock);
+
+	TRACE("nPackets: %zu", nPackets);
+
+	for (size_t cnt = 0; cnt < nPackets; cnt++) {
+		while (!dev->rxAcknowledged[dev->nextRxDesc]) {
+			(void)condWait(dev->rxAckCond, dev->rxLock, 0);
+		}
+
+		dev->rxAcknowledged[dev->nextRxDesc] = false;
+
+		volatile spfi_rxDesc_t *desc = &dev->rxDesc[dev->nextRxDesc];
+
+		/* Interrupt after each packet received */
+		desc->ctrl = SPFI_RX_DESC_IE;
+		if (dev->nextRxDesc == SPFI_RX_DESC_CNT - 1) {
+			/* Wrap around */
+			desc->ctrl |= SPFI_RX_DESC_WR;
+		}
+
+		memset((void *)dev->rxBuff[dev->nextRxDesc], 0, sizeof(dev->rxBuff[dev->nextRxDesc]));
+		uintptr_t pa = va2pa((void *)dev->rxBuff[dev->nextRxDesc]);
+		if ((pa & ~SPFI_ADDR_MASK) != 0) {
+			LOG_ERROR("DMA addr 0x%" PRIxPTR "exceeds 32-bit limit", pa);
+			return -EINVAL;
+		}
+		desc->addr = pa;
+
+		/* Everything is set up, enable descriptor */
+		desc->ctrl |= SPFI_RX_DESC_EN;
+
+		dev->nextRxDesc = (dev->nextRxDesc + 1) % SPFI_RX_DESC_CNT;
+	}
+
+	*firstDesc = (dev->nextRxDesc + SPFI_RX_DESC_CNT - nPackets) % SPFI_RX_DESC_CNT;
+
+	/* Enable receiver */
+	dev->mmio->vc[0].ctrl |= (SPFI_DCCTRL_RE);
+
+	(void)mutexUnlock(dev->rxLock);
+	(void)mutexUnlock(dev->rxConfLock);
+
+	return nPackets;
 }
 
 
@@ -231,7 +323,7 @@ void spfi_thread(void *arg)
 	msg_rid_t rid;
 
 	for (;;) {
-		while (msgRecv(spfi_common.spwOid.port, &msg, &rid) < 0) {
+		while (msgRecv(spfi_common.oid.port, &msg, &rid) < 0) {
 		}
 
 		switch (msg.type) {
@@ -251,7 +343,7 @@ void spfi_thread(void *arg)
 				break;
 		}
 
-		msgRespond(spfi_common.spwOid.port, &msg, rid);
+		msgRespond(spfi_common.oid.port, &msg, rid);
 	}
 }
 
@@ -259,6 +351,20 @@ void spfi_thread(void *arg)
 static int spfi_createResources(spfi_dev_t *dev, addr_t pbase)
 {
 	dev->mmio = MAP_FAILED;
+	dev->ctrlLock = (handle_t)-1;
+	dev->txLock = (handle_t)-1;
+	dev->rxLock = (handle_t)-1;
+	dev->txIrqLock = (handle_t)-1;
+	dev->rxConfLock = (handle_t)-1;
+	dev->cond = (handle_t)-1;
+	dev->rxAckCond = (handle_t)-1;
+	for (size_t i = 0; i < SPFI_RX_DESC_CNT; i++) {
+		dev->rxAcknowledged[i] = true;
+	}
+	dev->rxBuff = MAP_FAILED;
+	dev->txBuff = MAP_FAILED;
+	dev->rxDesc = MAP_FAILED;
+	dev->txDesc = MAP_FAILED;
 
 	uintptr_t base = (pbase & ~(_PAGE_SIZE - 1));
 	dev->mmio = mmap(NULL, _PAGE_SIZE, PROT_WRITE | PROT_READ, MAP_DEVICE | MAP_PHYSMEM | MAP_ANONYMOUS, -1, (off_t)base);
@@ -268,12 +374,74 @@ static int spfi_createResources(spfi_dev_t *dev, addr_t pbase)
 
 	dev->mmio = (void *)((uintptr_t)dev->mmio + pbase - base);
 
+	// if (spw_buffersAlloc(dev) < 0) {
+	// 	return -1;
+	// }
+
+	if (mutexCreate(&dev->ctrlLock) < 0) {
+		return -1;
+	}
+
+	if (mutexCreate(&dev->txLock) < 0) {
+		return -1;
+	}
+
+	if (mutexCreate(&dev->rxLock) < 0) {
+		return -1;
+	}
+
+	if (mutexCreate(&dev->txIrqLock) < 0) {
+		return -1;
+	}
+
+	if (mutexCreate(&dev->rxConfLock) < 0) {
+		return -1;
+	}
+
+	if (condCreate(&dev->cond) < 0) {
+		return -1;
+	}
+
+	if (condCreate(&dev->rxAckCond) < 0) {
+		return -1;
+	}
+
 	return 0;
 }
 
 
 static void spfi_cleanupResources(spfi_dev_t *dev)
 {
+	if (dev->rxAckCond != (handle_t)-1) {
+		resourceDestroy(dev->rxAckCond);
+	}
+
+	if (dev->cond != (handle_t)-1) {
+		resourceDestroy(dev->cond);
+	}
+
+	if (dev->rxConfLock != (handle_t)-1) {
+		resourceDestroy(dev->rxConfLock);
+	}
+
+	if (dev->txIrqLock != (handle_t)-1) {
+		resourceDestroy(dev->txIrqLock);
+	}
+
+	if (dev->rxLock != (handle_t)-1) {
+		resourceDestroy(dev->rxLock);
+	}
+
+	if (dev->txLock != (handle_t)-1) {
+		resourceDestroy(dev->txLock);
+	}
+
+	if (dev->ctrlLock != (handle_t)-1) {
+		resourceDestroy(dev->ctrlLock);
+	}
+
+	// spw_buffersFree(dev);
+
 	if (dev->mmio != MAP_FAILED) {
 		(void)munmap((void *)dev->mmio, _PAGE_SIZE);
 	}
@@ -343,7 +511,7 @@ int main(void)
 {
 	oid_t oid;
 
-	if (portCreate(&spfi_common.spwOid.port) < 0) {
+	if (portCreate(&spfi_common.oid.port) < 0) {
 		LOG_ERROR("Failed to create port\n");
 		return EXIT_FAILURE;
 	}
@@ -354,19 +522,19 @@ int main(void)
 	}
 
 	if (spfi_init() < 0) {
-		portDestroy(spfi_common.spwOid.port);
+		portDestroy(spfi_common.oid.port);
 		LOG_ERROR("Failed to initialize SpaceFibre\n");
 		return EXIT_FAILURE;
 	}
 
-	if (spfi_createDevs(&spfi_common.spwOid) < 0) {
-		portDestroy(spfi_common.spwOid.port);
+	if (spfi_createDevs(&spfi_common.oid) < 0) {
+		portDestroy(spfi_common.oid.port);
 		LOG_ERROR("Failed to create SpaceFibre devices");
 		return EXIT_FAILURE;
 	}
 
 	for (int i = 1; i < SPFI_THREADS_NO - 1; i++) {
-		beginthread(spfi_thread, SPFI_PRIO, spfi_common.spfiStack[i], SPFI_STACKSZ, NULL);
+		beginthread(spfi_thread, SPFI_PRIO, spfi_common.stack[i], SPFI_STACKSZ, NULL);
 	}
 
 	LOG("initialized");
