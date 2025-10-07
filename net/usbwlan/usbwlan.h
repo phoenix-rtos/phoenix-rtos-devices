18f44e4deeefb02f306984634161453d74d2f247
julianuziemblo <julian.uziemblo@phoenix-rtos.com>
2025-10-07 12:04:27 +0200

    add usbwlan driver
    Driver for vendor-specific usbwlan devices
    Only available for Sterling LWB5+ USB Dongle for now.
    
    JIRA: RTOS-1122

---
+++ net/usbwlan/usbwlan.h
@@ -0,0 +1,37 @@
+/*
+ * Phoenix-RTOS
+ *
+ * USB WLAN driver
+ *
+ * Copyright 2025 Phoenix Systems
+ * Author: Julian Uziembło
+ *
+ * %LICENSE%
+ */
+
+#ifndef _USBWLAN_H_
+#define _USBWLAN_H_
+
+#include <stdint.h>
+
+
+typedef struct {
+	/* clang-format off */
+	enum { usbwlan_dl, usbwlan_ctrlIn, usbwlan_ctrlOut, usbwlan_regRead, usbwlan_regWrite } type;
+	/* clang-format on */
+
+	union {
+		struct {
+			uint8_t cmd;
+			uint16_t wIndex;
+		} dl;
+
+		struct {
+			uint8_t cmd;
+			uint32_t regaddr;
+		} reg;
+	};
+} usbwlan_i_t;
+
+
+#endif /* _USBWLAN_H_ */
