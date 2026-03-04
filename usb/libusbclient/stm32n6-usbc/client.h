22523756b9e4b149942d20640d0a9369a58072d4
rmikielis <rafal.mikielis@phoenix-rtos.com>
2026-03-04 17:25:45 +0100

    stm32n6: libusbclient
    add support for CDC-ACM device mode
    
    RTOS-1207

--- usb/libusbclient/stm32n6-usbc/client.h
+++ usb/libusbclient/stm32n6-usbc/client.h
@@ -1,10 +1,14 @@
 /*
  * Phoenix-RTOS
  *
- * USB Client Header for STM32N6 (DWC2 OTG HS) - Device Mode
+ * libusbclient (STM32 N6)
+ *
+ * Device-side CDC ACM driver
  *
  * Copyright 2025 Phoenix Systems
- * Author: Olaf Czerwinski, Radosław Szewczyk
+ * Author: Radosław Szewczyk, Rafał Mikielis
+ *
+ * This file is part of Phoenix-RTOS.
  *
  * %LICENSE%
  */
@@ -16,48 +20,70 @@
 #include <usbclient.h>
 #include <stdint.h>
 #include <sys/mman.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/msg.h>
+#include <sys/threads.h>
+#include <sys/interrupt.h>
+#include <errno.h>
 
-#include "phy.h"
 #include "registers.h"
 
+#define EP0_TX_BUFFER_SIZE 0x80U
+#define EP0_RX_BUFFER_SIZE 0x40U
+#define USB_BUFFER_SIZE    0x1000
+#define USB_OTG_TIMEOUT    0xF000000UL
 
-#define USB_BUFFER_SIZE 0x1000
+#define USB_SPEED_FS 0x2U
+#define USB_SPEED_HS 0x0U
 
-// #define OFF_DIEPTXF1 0x104
+#define DEVICE_SPEED USB_SPEED_FS
 
 /* States */
-#define DC_DEFAULT    0
-#define DC_ADDRESS    1
-#define DC_CONFIGURED 2
+#define DC_DEFAULT    0U
+#define DC_ADDRESS    1U
+#define DC_CONFIGURED 2U
 
 /* Operations */
-#define DC_OP_NONE      0
-#define DC_OP_INIT      1
-#define DC_OP_EXIT      2
-#define DC_OP_RCV_ENDP0 3
-#define DC_OP_RCV_ERR   4
+#define DC_OP_NONE      0U
+#define DC_OP_INIT      1U
+#define DC_OP_EXIT      2U
+#define DC_OP_RCV_ENDP0 3U
+#define DC_OP_RCV_ERR   4U
 
 /* Limits */
-#define ENDPOINTS_NUMBER 9
-#define ENDPOINTS_DIR_NB 2
+#define ENDPOINTS_NUMBER   9U
+#define ENDPOINTS_DIR_NB   2U
+#define MAX_PACKET_SIZE_FS 0x40UL
+#define MAX_PACKET_SIZE_HS 0x200UL
 
 /* FIFO Offsets */
 #define FIFO_BASE_OFF (0x1000 / 4)
 #define FIFO_SIZE     (0x1000UL)
 
 /* FIFO RX, TX Configuration */
-#define MAX_PCKT_SZ_EPO_TX_WORDS (0x40)   // 64 words
-#define RX_FIFO_DEPTH_WORDS      (0x200)  // 512 words - required minumim is 28 words (for max 64B EP0)
-#define TX0FSA                   RX_FIFO_DEPTH_WORDS
-#define TX0FD                    MAX_PCKT_SZ_EPO_TX_WORDS
-#define MAX_PCKT_SZ_EP0_TX_B     MAX_PCKT_SZ_EPO_TX_WORDS * 4
+#define RX_FIFO_DEPTH (0x200UL) /* 512 words - required minumim is 28 words (for max 64B EP0) */
+#define TX0FD         (0x40UL)  /* 64 words - EP0 IN */
+#define TX1FD         (0x10UL)  /* 16 words - CDC Control Interrupt IN */
+#define TX2FD         (0x180UL) /* 384 words - CDC Data Bulk IN */
+#define TX0FSA        RX_FIFO_DEPTH
+#define TX1FSA        (TX0FSA + TX0FD)
+#define TX2FSA        (TX1FSA + TX1FD)
 
-#define DIEPINTxWrMsk     0x297F
-#define DOEPINTxWrMsk     0xF17F
-#define DIEPCTL_MPSIZ_Msk 0x7FF
+#define DIEPINTxWrMsk     0x297fUL
+#define DOEPINTxWrMsk     0xf17fUL
+#define DIEPCTL_MPSIZ_Msk 0x7ff
+#define GINTSTSWrMsk      0xf8f0fc0aUL
 
+#define IS_IRQ(REG, IRQ) (((REG >> IRQ) & 1U) == 1U)
 
 #define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
+
+#define DAINTMSK_IN(x)  (1UL << x)
+#define DAINTMSK_OUT(x) (1UL << (x + 16UL))
+
 
 typedef struct
 {
@@ -71,65 +97,79 @@ typedef struct
 
 
 typedef struct _usb_dc_t {
-	volatile uint32_t *base;
-
-	uint32_t status;
-	uint32_t dev_addr;
-	volatile uint32_t setupstat;
-
-	uint8_t connected;
 
 	void *ctxUser;
 	void (*cbEvent)(int, void *);
 	int (*cbClassSetup)(const usb_setup_packet_t *, void *, unsigned int, void *);
+	int currEvent, prevEvent;
 
+	volatile uint32_t *base;
+
+	uint32_t devAddr;
+
+	uint8_t connected;
+	uint8_t enumSpeed;
+	uint8_t isPortOpened;
+
+	int threadNum;
 	handle_t irqCond;
 	handle_t irqLock;
 	handle_t endp0Cond;
 	handle_t endp0Lock;
 	handle_t inth;
 
-	volatile int runIrqThread;
-	volatile int pending_event;
-	volatile int endptFailed;
+	semaphore_t semBulkTx, semBulkRx;
 
-	volatile uint8_t op;
-	usb_setup_packet_t setup;
+	volatile uint8_t runIrqThread;
+
+	usb_setup_packet_t setupPacket;
+	uint8_t ep0buffTX[EP0_TX_BUFFER_SIZE];
+	uint8_t ep0buffRX[EP0_RX_BUFFER_SIZE];
 
 	uint8_t deviceState;
 	uint8_t ep0State;
 
-	usb_endpointtypedef_t epIn[ENDPOINTS_NUMBER];
-	usb_endpointtypedef_t epOut[ENDPOINTS_NUMBER];
-
-	uint32_t ep0DataLen;
+	uint32_t pending_event;
+	uint32_t daintClear;
+	uint32_t irqPendingDIEPINT[ENDPOINTS_NUMBER];
+	uint32_t irqPendingDOEPINT[ENDPOINTS_NUMBER];
+	uint32_t diepmsk;
 } usb_dc_t;
-
 
 typedef struct _usb_buffer_t {
 	uint8_t *vBuffer;
-	addr_t pBuffer;
-	int16_t len;
+	uint16_t size;
+	uint8_t *head, *tail;
 } usb_buffer_t;
 
 
-/**
+typedef struct usb_xfer_desc {
+	uint16_t len;
+	uint8_t *head, *tail;
+} usb_xfer_desc_t;
+
+/*
  * Endpoint structure for stm32n6 usb
  * Params:
  *  - xfer_buf - pointer to buffer with received / transmit data
+ *  - xfer_size - size of the transaction
  *  - xfer_len - length of the buffer
- *  - xfer_count - partial transfer lenth in case of multipacket transfer
+ *  - xfer_count - partial transfer length in case of multipacket transfer
  *  - is_in - (1 - true / 0 - false)  - is it IN endpoint
  */
 typedef struct _stm32n6_endpt_data_t {
+	/* Current transfer parameters*/
 	uint8_t *xfer_buf;
 	uint32_t xfer_len;
 	uint32_t xfer_count;
+	uint32_t xfer_size;
+	volatile uint8_t xfer_active;
+	/* Endpoint static data */
+	usb_buffer_t dataBuf;
 	uint8_t is_in;
 	uint8_t epNum;
 	uint8_t type;
 	uint32_t maxpacket;
-	uint32_t xfer_size;
 } stm32n6_endpt_data_t;
 
 
@@ -142,7 +182,6 @@ typedef struct _stm32n6_endpt_t {
 typedef struct {
 	char *setupMem;
 	stm32n6_endpt_t endpts[ENDPOINTS_NUMBER];
-	uint32_t bufforek[ENDPOINTS_DIR_NB];
 } usb_common_data_t;
 
 
@@ -150,6 +189,7 @@ enum {
 	USB_EP_DIR_IN,
 	USB_EP_DIR_OUT
 };
+
 
 enum {
 	USB_EP_TYPE_CTRL = 0U,
@@ -163,6 +203,7 @@ enum {
 #define USBD_STATE_ADDRESSED  0x02U
 #define USBD_STATE_CONFIGURED 0x03U
 #define USBD_STATE_SUSPENDED  0x04U
+#define USBD_STATE_ERROR      0xFFU
 
 
 /*  EP0 State */
@@ -174,38 +215,53 @@ enum {
 #define USBD_EP0_STATUS_OUT 0x05U
 #define USBD_EP0_STALL      0x06U
 
-
-int usbclient_init(usb_desc_list_t *desList);
-int usbclient_send(int endpt, const void *data, unsigned int len);
-int usbclient_receive(int endpt, void *data, unsigned int len);
-int usbclient_destroy(void);
-
-void usbclient_setUserContext(void *ctxUser);
-void usbclient_setEventCallback(void (*cbEvent)(int, void *));
-void usbclient_setClassCallback(int (*cbClassSetup)(const usb_setup_packet_t *, void *, unsigned int, void *));
+/* Received packet status */
+#define RX_PKTSTS_GOUT_NAK    0x1U
+#define RX_PKTSTS_OUT_RXED    0x2U
+#define RX_PKTSTS_OUT_CMPLT   0x3U
+#define RX_PKTSTS_SETUP_CMPLT 0x4U
+#define RX_PKTSTS_SETUP_RXED  0x6U
 
 
-extern int ctrl_init(usb_common_data_t *usb_data_in, usb_dc_t *dc_in);
-extern int ctrl_hfIrq(void);
-extern void ctrl_lfIrq(void);
+void usbclient_buffDestory(void *addrs, uint32_t size);
+void *usbclient_allocBuff(uint32_t size);
+
+extern void ctrl_init(usb_common_data_t *usb_data_in, usb_dc_t *dc_in);
+extern void ctrl_hifiq_handler(uint32_t *irqStatus);
+extern void ctrl_lifiq_handler(uint32_t *irqStatus);
 extern void ctrl_setAddress(uint32_t addr);
-extern int ctrl_execTransfer(int endpt, uint8_t *virtAddr, int nBytes);
 
-extern int clbc_init(usb_common_data_t *usb_data_in, usb_dc_t *dc_in);
+extern void clbc_init(usb_common_data_t *usb_data_in, usb_dc_t *dc_in);
 extern void clbc_reset(void);
+extern int clbc_resetUSBSS(void);
 extern void clbc_enumdne(void);
-extern void clbc_dataInStage(uint8_t epNum);
 extern void clbc_ep0OutStart(void);
-extern void clbc_endptInit(void);
-extern void clbc_rxFifoData(int debug);
-extern void clbc_flushTxFifo(uint32_t num);
-extern void clbc_sendEpData(int ep, uint8_t *virtAddr, int nBytes);
-extern int clbc_epTransmit(uint8_t epNum, uint8_t *dataBuf, uint32_t len);
+extern void clbc_epStall(stm32n6_endpt_t *ep);
+extern int clbc_setDevMode(void);
+extern void clbc_rxFifoData(void);
+extern int clbc_flushTxFifo(uint8_t num);
+extern int clbc_flushRxFifo(void);
+extern void clbc_sendEpData(uint8_t epNum);
+extern void clbc_receiveEpData(uint8_t *dest, uint16_t nBytes);
+extern void clbc_epTransmit(uint8_t epNum, uint8_t *dataBuf, uint32_t len);
 
+/* RX on control endpoint */
+extern int clbc_ep0Receive(usb_xfer_desc_t *desc);
+
+/* RX on data endpoint*/
+extern void clbc_epReceive(uint8_t epNum, uint8_t *dataBuf, uint32_t len);
+extern void clbc_epReceiveCont(uint8_t epNum);
+
+extern void _clbc_USBRST(void);
+extern void _clbc_ENUMDNE(void);
+extern void _clbc_OEPINT(void);
+extern void _clbc_IEPINT(void);
 
 extern int desc_init(usb_desc_list_t *desList, usb_common_data_t *usb_data_in, usb_dc_t *dc_in);
 extern int desc_setup(const usb_setup_packet_t *setup);
 extern int desc_classSetup(const usb_setup_packet_t *setup);
+extern void desc_epActivation(stm32n6_endpt_data_t *ep);
+extern void desc_epDeactivation(stm32n6_endpt_data_t *ep);
 
 
 #endif
