a9682fce194f0534658d1c6c9e63b1c31bbdd3eb
Grzegorz <grzegorz.mazurczak@phoenix-rtos.com>
2026-04-01 19:10:05 +0200

    sensors: add support for optical flow sensor
    Changes:
    -> add new sensor type: optical flow,
    -> add new fields in time-of-flight sensor type.
    
    YT: PP-458

--- sensors/client-include/libsensors/client.h
+++ sensors/client-include/libsensors/client.h
@@ -34,6 +34,7 @@
 #define SENSOR_TYPE_FSS      (1 << 8)
 #define SENSOR_TYPE_STAR     (1 << 9)
 #define SENSOR_TYPE_TOF      (1 << 10)
+#define SENSOR_TYPE_OPTFLOW  (1 << 11)
 
 
 typedef unsigned int sensor_type_t;
@@ -193,8 +194,20 @@ typedef struct {
 
 typedef struct {
 	uint32_t devId;
-	uint32_t dist; /* Distance [mm] to the closest object */
+	uint32_t dist;   /* Distance [mm] to the closest object */
+	uint8_t valid;   /* Measurement validity indicator */
+	uint8_t quality; /* Distance measurement quality (0-255), lower index means higher accuracy */
+	uint8_t reserved[2];
 } tof_data_t;
+
+
+typedef struct {
+	uint32_t devId;
+	int32_t vel[2];  /* Flow velocity (velocity [mm / s] per distance [m]) measurement for x and y sensor axis [mm/(s * m)] */
+	uint8_t valid;   /* Measurement validity indicator */
+	uint8_t quality; /* Velocity measurement quality (0-255), lower index means higher accuracy */
+	uint8_t reserved[2];
+} opticalFlow_data_t;
 
 
 /* Event data gets from sensor manager */
@@ -214,6 +227,7 @@ typedef struct {
 		fss_data_t fss;
 		starTracker_data_t st;
 		tof_data_t tof;
+		opticalFlow_data_t optFlow;
 	};
 } sensor_event_t;
 
