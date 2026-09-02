e42a3cdf5dc44418a5321e1c05630c039f416792
Amelia Waszkowska <amelia.waszkowska@phoenix-rtos.com>
2026-09-02 14:32:37 +0200

    storage/flashdrv/drv/grlib-spimctrl: add interfaces to change properties of controller e.g. SPI mode
       YT: CSAT-273

--- storage/flashdrv/drv/grlib-spimctrl/spimctrl.h
+++ storage/flashdrv/drv/grlib-spimctrl/spimctrl.h
@@ -17,12 +17,28 @@
 #include <stdint.h>
 #include <sys/types.h>
 
+#define FLASH0_AHB_ADDR 0xC0000000
+
+#ifndef SPIMCTRL0_BASE
+#define SPIMCTRL0_BASE ((void *)0xFFF00000)
+#endif
+
 
 struct spimctrl {
 	volatile uint32_t *base;
 
-	uint8_t ear;
+	uint8_t ear; /* extended address register (3-byte mode) */
+	uint8_t extendedAddress; /* 4 byte mode  */
 };
+
+
+typedef enum {
+	MEXTENDED_SPI,
+	MDUAL_OUTPUT,
+	MDSPI,
+	MQUAD_OUTPUT,
+	MQSPI
+}SPIMode_t;
 
 
 struct xferOp {
@@ -51,7 +67,28 @@ void spimctrl_reset(const struct spimctrl *spimctrl);
 int spimctrl_init(struct spimctrl *spimctrl, addr_t mctrlBase);
 
 
+/* Destroy spimctrl instance */
 void spimctrl_destroy(struct spimctrl *spimctrl);
+
+
+/* Select SPI mode */
+int spimctrl_spiMode(const struct spimctrl *spimctrl, SPIMode_t spi_mode);
+
+
+/* Enable alternate scaler */
+void spimctrl_enableAlternateScaler(volatile uint32_t *spimctrlBase);
+
+
+/* Disable alternate scaler */
+void spimctrl_disableAlternateScaler(volatile uint32_t *spimctrlBase);
+
+
+/* Set dummy byte */
+void spimctrl_setDummyByte(volatile uint32_t *spimctrlBase);
+
+
+/* Set dummy cyclec*/
+int spimctrl_setDummyCycles(volatile uint32_t *spimctrlBase, uint8_t numCycles);
 
 
 #endif
