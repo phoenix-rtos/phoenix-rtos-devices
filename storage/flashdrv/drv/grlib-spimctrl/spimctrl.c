4a7c9d44a1583c8a0e13830c17e9d9b6891182c8
Amelia Waszkowska <amelia.waszkowska@phoenix-rtos.com>
2026-09-02 14:35:21 +0200

    storage/flashdrv/drv/grlib-spimctrl: add interfaces to change properties of controller e.g. SPI mode
       YT: CSAT-273

--- storage/flashdrv/drv/grlib-spimctrl/spimctrl.c
+++ storage/flashdrv/drv/grlib-spimctrl/spimctrl.c
@@ -19,9 +19,23 @@
 
 #include "spimctrl.h"
 
+/* Configuration register */
+
+#define DCYCLES (0xFUL << 8)
+#define DSPI (1 << 12)
+#define QSPI (1 << 13)
+#define EXTENDED_ADDRESS (1 << 14)
+#define DBYTE (1 << 15)
+#define DOUT (1 << 16)
+#define QOUT (1 << 17)
+#define DIN (1 << 18)
+#define QIN (1 << 19)
+#define XIP (1 << 20)
+
 /* Control register */
 
 #define USR_CTRL (1 << 0)
+#define EAS (1 << 2)
 #define CHIP_SEL (1 << 3)
 #define CORE_RST (1 << 4)
 
@@ -61,6 +75,87 @@ static int spimctrl_ready(const struct spimctrl *spimctrl)
 	uint32_t val = (*(spimctrl->base + flash_stat) & (INITIALIZED | OPER_DONE));
 
 	return (val == INITIALIZED) ? 1 : 0;
+}
+
+static void spimctrl_addressMode(const struct spimctrl *spimctrl)
+{
+	// if (spimctrl->extendedAddress) {
+	// 	*(spimctrl->base + flash_cfg) |= EXTENDED_ADDRESS;
+	// }
+	// else {
+	// 	*(spimctrl->base + flash_cfg) &= ~EXTENDED_ADDRESS;
+	// }
+
+	*(spimctrl->base + flash_cfg) |= EXTENDED_ADDRESS;
+
+}
+
+
+int spimctrl_spiMode(const struct spimctrl *spimctrl, SPIMode_t spi_mode)
+{
+	int res = 0;
+	switch (spi_mode)
+	{
+		case MEXTENDED_SPI:
+			*(spimctrl->base + flash_cfg) &= ~(DSPI | DOUT | QOUT | QSPI | DIN | QIN);
+			break;
+		case MDUAL_OUTPUT:
+			*(spimctrl->base + flash_cfg) &= ~(DSPI | QOUT | QSPI | DIN | QIN);
+			*(spimctrl->base + flash_cfg) |= DOUT;
+			break;
+		case MDSPI:
+			*(spimctrl->base + flash_cfg) &= ~(DOUT | QOUT | QSPI | DIN | QIN);
+			*(spimctrl->base + flash_cfg) |= DSPI;
+			break;
+		case MQUAD_OUTPUT:
+			*(spimctrl->base + flash_cfg) &= ~(DSPI | DOUT | QSPI | DIN | QIN);
+			*(spimctrl->base + flash_cfg) |= QOUT;
+			break;
+		case MQSPI:
+			*(spimctrl->base + flash_cfg) &= ~(DSPI | QOUT | DOUT | DIN | QIN);
+			*(spimctrl->base + flash_cfg) |= QSPI;
+			break;
+		
+		default:
+			res = -EINVAL;
+			break;
+	}
+
+	return res;
+}
+
+
+void spimctrl_enableAlternateScaler(volatile uint32_t *spimctrlBase)
+{
+	*(spimctrlBase + flash_ctrl) |= EAS;
+}
+
+
+void spimctrl_disableAlternateScaler(volatile uint32_t *spimctrlBase)
+{
+	*(spimctrlBase + flash_ctrl) &= ~EAS;
+}
+
+
+void spimctrl_setDummyByte(volatile uint32_t *spimctrlBase)
+{
+	*(spimctrlBase + flash_ctrl) |= DBYTE;
+}
+
+
+int spimctrl_setDummyCycles(volatile uint32_t *spimctrlBase, uint8_t numCycles)
+{
+	int res = 0;
+	if (numCycles < DCYCLES + 1) {
+		*(spimctrlBase + flash_ctrl) &= ~DCYCLES;
+		*(spimctrlBase + flash_ctrl) &= ~DBYTE;
+		*(spimctrlBase + flash_ctrl) |= ((numCycles & 0xFUL) << 8);
+	}
+	else {
+		res = -EINVAL;
+	}
+
+	return res;
 }
 
 
@@ -150,6 +245,9 @@ int spimctrl_init(struct spimctrl *spimctrl, addr_t mctrlBase)
 
 	/* Reset core */
 	spimctrl_reset(spimctrl);
+
+	/* Set address mode */
+	spimctrl_addressMode(spimctrl);
 
 	return 0;
 }
